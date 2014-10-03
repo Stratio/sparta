7d0b899dcc6ba2822a9f04b6b47fc5244ee54e23
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-10-03 11:57:37 +0200

    Changed name of AggregationPoliciesConfiguration to AggregationPoliciesDto

--- driver/src/main/scala/com/stratio/sparkta/driver/dto/AggregationPoliciesDto.scala
+++ driver/src/main/scala/com/stratio/sparkta/driver/dto/AggregationPoliciesDto.scala
@@ -13,12 +13,12 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-package com.stratio.sparkta.driver.configuration
+package com.stratio.sparkta.driver.dto
 
 /**
  * Created by ajnavarro on 2/10/14.
  */
-case class AggregationPoliciesConfiguration(name: String = "default",
+case class AggregationPoliciesDto(name: String = "default",
                                             receiver: String = "kafka",
                                             receiverConfiguration: Map[String, String],
                                             eventParser: Option[String],
