6c5bde11e49d96489bfe15018fb5c966f4e8dbbf
Antonio Jesus Navarro Perez <ajnavarro@paradigmatecnologico.com>
2014-10-27 11:03:58 +0100

    added to default constructor all bucketTypes

--- plugins/bucketer-geoHash/src/main/scala/com/stratio/sparkta/plugin/bucketer/geohash/GeoHashBucketer.scala
+++ plugins/bucketer-geoHash/src/main/scala/com/stratio/sparkta/plugin/bucketer/geohash/GeoHashBucketer.scala
@@ -5,7 +5,7 @@
  * you may not use this file except in compliance with the License.
  * You may obtain a copy of the License at
  *
- *         http://www.apache.org/licenses/LICENSE-2.0
+ * http://www.apache.org/licenses/LICENSE-2.0
  *
  * Unless required by applicable law or agreed to in writing, software
  * distributed under the License is distributed on an "AS IS" BASIS,
@@ -51,7 +51,10 @@ case class GeoHashBucketer(override val bucketTypes: Seq[BucketType] =
                              precision6,
                              precision7,
                              precision8,
-                             precision9)) extends Bucketer {
+                             precision9,
+                             precision10,
+                             precision11,
+                             precision12)) extends Bucketer {
 
   override def bucketForWrite(value: io.Serializable): Map[BucketType, Seq[io.Serializable]] = {
     //TODO temporal data treatment
