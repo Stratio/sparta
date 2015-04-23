7a351963f8f4dbc5c9da76689d7b5bdb48134123
arinconstrio <arincon@stratio.com>
2015-04-23 18:03:51 +0200

    scalastyle

--- driver/src/test/scala/com/stratio/sparkta/driver/test/service/RawDataStorageServiceSpec.scala
+++ driver/src/test/scala/com/stratio/sparkta/driver/test/service/RawDataStorageServiceSpec.scala
@@ -1,26 +1,44 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
 package com.stratio.sparkta.driver.test.service
 
+import java.io.{File, Serializable => JSerializable}
 import scala.collection.mutable
 import scala.language.postfixOps
 
 import org.apache.spark.rdd.RDD
-
-import org.apache.spark.streaming._
-import org.apache.spark.streaming.dstream.{DStream, InputDStream}
-
-import org.apache.spark.{SparkContext, SparkConf}
 import org.apache.spark.sql.SQLContext
-import java.io.{Serializable => JSerializable, File}
+import org.apache.spark.streaming._
+import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.{SparkConf, SparkContext}
+import org.scalatest._
+
 import com.stratio.sparkta.driver.service.RawDataStorageService
 import com.stratio.sparkta.sdk.Event
-
-import org.scalatest._
 
 /**
  * Created by arincon on 21/04/15.
  */
-class RawDataStorageServiceSpec extends WordSpecLike  with BeforeAndAfterAll with Matchers{
-  val path="testPath"
+class RawDataStorageServiceSpec extends WordSpecLike with BeforeAndAfterAll with Matchers {
+
+  val WindowDuration: Long = 100
+  val SleepTime: Long = 500
+  val ExpectedResult: Long = 2000
+  val path = "testPath"
 
   override def afterAll {
     val file = new File(path)
@@ -31,16 +49,16 @@ class RawDataStorageServiceSpec extends WordSpecLike  with BeforeAndAfterAll wit
     .setAppName(this.getClass.getSimpleName + "" + System.currentTimeMillis())
     .setMaster("local[2]")
   val sc = new SparkContext(myConf)
-  val ssc= new StreamingContext(sc, Duration.apply(100))
-  val sqlCtx: SQLContext = new SQLContext(sc)
 
+  val ssc = new StreamingContext(sc, Duration.apply(WindowDuration))
+  val sqlCtx: SQLContext = new SQLContext(sc)
 
   val events = mutable.Queue[RDD[Event]]()
   val getEvents: Seq[Event] = (0 until 1000) map (i => new Event(Map("key" + i -> System.currentTimeMillis()
     .asInstanceOf[JSerializable]), Some("myRaw event data here")))
 
   def deleteParquetFiles(file: File): Unit = {
-    if(file.exists() && file.isDirectory)
+    if (file.exists() && file.isDirectory)
       Option(file.listFiles).map(_.toList).getOrElse(Nil).foreach(deleteParquetFiles(_))
     file.delete()
   }
@@ -53,19 +71,17 @@ class RawDataStorageServiceSpec extends WordSpecLike  with BeforeAndAfterAll wit
       ssc.start()
 
       events += sc.makeRDD(getEvents)
-      Thread.sleep(500)
+      Thread.sleep(SleepTime)
       events += sc.makeRDD(getEvents)
-      Thread.sleep(800)
+      Thread.sleep(SleepTime)
 
       ssc.stop()
 
       val newSqlCtx = new SQLContext(new SparkContext(myConf))
       val pqFile = newSqlCtx.parquetFile(path)
 
-      pqFile.count() should be(2000)
+      pqFile.count() should be(ExpectedResult)
     }
-
-
   }
 }
 
