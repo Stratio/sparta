d0b77944f3ddb4ce369bc25b374f34681ad37817
Alvaro Nistal <alvaro.nistal@gmail.com>
2015-05-12 13:07:23 +0200

    Proper DataCube spec using Sparkt's TestSuiteBase

--- aggregator/src/test/scala/com/stratio/sparkta/aggregator/DataCubeSpec.scala
+++ aggregator/src/test/scala/com/stratio/sparkta/aggregator/DataCubeSpec.scala
@@ -17,60 +17,80 @@
 package com.stratio.sparkta.aggregator
 
 import java.io.{Serializable => JSerializable}
-import scala.collection.mutable
-
-import org.apache.spark._
-import org.apache.spark.rdd.RDD
-import org.apache.spark.streaming._
-import org.junit.runner.RunWith
-import org.scalatest.FlatSpec
-import org.scalatest.junit.JUnitRunner
 
 import com.stratio.sparkta.plugin.bucketer.passthrough.PassthroughBucketer
 import com.stratio.sparkta.plugin.operator.count.CountOperator
 import com.stratio.sparkta.sdk._
+import org.apache.spark.streaming.TestSuiteBase
+import org.scalatest.{FeatureSpec, FlatSpec}
 
-@RunWith(classOf[JUnitRunner])
-class DataCubeSpec  extends FlatSpec{
+/**
+ * Tests over DataCube operations.
+ * @author anistal
+ */
+class DataCubeSpec extends TestSuiteBase {
 
-  "DataCubeSpec" should "process the events queued" in {
+  val PreserverOrder = false
 
-    val myConf = new SparkConf()
-      .setAppName(this.getClass.getSimpleName + "" + System.currentTimeMillis())
-      .setMaster("local[2]").set("spark.streaming.clock", "org.apache.spark.streaming.util.ManualClock")
-    val sc = new SparkContext(myConf)
-    val ssc: StreamingContext = new StreamingContext(sc, Seconds(2))
-    val clock = new ClockWrapper(ssc)
+  test("""Given a rollup defined with:
+    - D = A dimension with name eventKey and a string value.
+    - B = A PassthroughBucketer applied to the dimension
+    - O = No operator for the rollup
+    - R = Rollup with D+B+O
 
-    def myDim(i: Int) = new Dimension("myKey" + i, new PassthroughBucketer().asInstanceOf[Bucketer])
+    This test should produce Seq[(Seq[DimensionValue], Map[String, JSerializable])] with values:
 
-    def getDimensions: Seq[Dimension] = (0 until 10) map (i => myDim(i))
+    List(
+     (List(DimensionValue(
+       Dimension(eventKey,PassthroughBucketer()),BucketType(identity,Map()),value1)),Map(eventKey -> value1)),
+     (List(DimensionValue(
+       Dimension(eventKey,PassthroughBucketer()),BucketType(identity,Map()),value2)),Map(eventKey -> value2)),
+     (List(DimensionValue(
+       Dimension(eventKey,PassthroughBucketer()),BucketType(identity,Map()),value3)),Map(eventKey -> value3)))
+         """) {
+    val bucketer = new PassthroughBucketer
+    val dimension = Dimension("eventKey", bucketer)
+    val operator = new CountOperator(Map())
+    val bucketType = new BucketType("identity")
+    val rollup = new Rollup(Seq(dimension -> bucketType), Seq(operator))
+    val dataCube = new DataCube(Seq(dimension), Seq(rollup))
 
-    def getComponents: Seq[(Dimension, BucketType)] = (0 until 1) map (i => (myDim(i), Bucketer.identity))
-
-    def getOperators: Seq[Operator] = (0 until 1) map (i => new CountOperator(Map("prop" -> "propValue"
-      .asInstanceOf[JSerializable])))
-
-    val myRollups: Seq[Rollup] = (0 until 2) map (i => new Rollup(components = getComponents, operators = getOperators))
-
-    val events = mutable.Queue[RDD[Event]]()
-    val myDimensions: Seq[Dimension] = getDimensions
-
-    val getEvents: Seq[Event] = (0 until 10) map (i => new Event(Map("key" + i -> System.currentTimeMillis()
-      .asInstanceOf[JSerializable]), Some("myRaw event data here")))
-
-
-    val dc: DataCube = new DataCube(myDimensions, myRollups)
-    val result = dc.setUp(ssc.queueStream(events))
-    result.foreach(i => i.print)
-
-    assert(result.size == 2)
-
-    ssc.start()
-    events += sc.makeRDD(getEvents)
-    clock.advance(2)
-
-    ssc.stop()
-
+    testOperation(
+      getEventInput,
+      dataCube.extractDimensionsStream,
+      getEventOutput,
+      PreserverOrder
+    )
   }
+
+  /**
+   * It gets a stream of data to test.
+   * @return a stream of data.
+   */
+  def getEventInput(): Seq[Seq[Event]] =
+    Seq(Seq(
+      Event(Map("eventKey" -> "value1")),
+      Event(Map("eventKey" -> "value2")),
+      Event(Map("eventKey" -> "value3"))
+    ))
+
+  /**
+   * The expected result to test the DataCube output.
+   * @return the expected result to test
+   */
+  def getEventOutput(): Seq[Seq[(Seq[DimensionValue], Map[String, JSerializable])]] =
+    Seq(Seq(
+      (Seq(DimensionValue(
+        Dimension("eventKey", new PassthroughBucketer), BucketType("identity", Map()), "value1")),
+        Map("eventKey" -> "value1")
+      ),
+      (Seq(DimensionValue(
+        Dimension("eventKey", new PassthroughBucketer), BucketType("identity", Map()), "value2")),
+        Map("eventKey" -> "value2")
+      ),
+      (Seq(DimensionValue(
+        Dimension("eventKey", new PassthroughBucketer), BucketType("identity", Map()), "value3")),
+        Map("eventKey" -> "value3")
+      )
+    ))
 }
