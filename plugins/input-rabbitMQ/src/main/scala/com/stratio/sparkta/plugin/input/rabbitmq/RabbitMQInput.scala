7e66b03390bc79d2f362407317b608d29a6c0d3e
Daniel Carroza <danielcarrozasantana@gmail.com>
2015-05-19 12:19:20 +0200

    Added rabbitmq-receiver repo as a submodule

--- plugins/input-rabbitMQ/src/main/scala/com/stratio/sparkta/plugin/input/rabbitmq/RabbitMQInput.scala
+++ plugins/input-rabbitMQ/src/main/scala/com/stratio/sparkta/plugin/input/rabbitmq/RabbitMQInput.scala
@@ -18,28 +18,23 @@ package com.stratio.sparkta.plugin.input.rabbitmq
 
 import java.io.{Serializable => JSerializable}
 
-import com.stratio.sparkta.sdk.Input._
-import com.stratio.sparkta.sdk.{Event, Input}
+import com.stratio.receiver.RabbitMQUtils
 import org.apache.spark.storage.StorageLevel
 import org.apache.spark.streaming.StreamingContext
 import org.apache.spark.streaming.dstream.DStream
-import com.stratio.sparkta.sdk.ValidatingPropertyMap._
 
-/**
- * Created by dcarroza on 4/17/15.
- */
-class RabbitMQInput(properties: Map[String, JSerializable]) extends Input(properties){
+import com.stratio.sparkta.sdk.Input._
+import com.stratio.sparkta.sdk.ValidatingPropertyMap._
+import com.stratio.sparkta.sdk.{Event, Input}
+
+class RabbitMQInput(properties: Map[String, JSerializable]) extends Input(properties) {
 
   val storageLevel = properties.getString("storageLevel", "MEMORY_AND_DISK_SER_2")
 
+  val port = 5672
+
   override def setUp(ssc: StreamingContext): DStream[Event] = {
-    val receiverStream =
-      ssc.receiverStream(
-        new RabbitMQReceiver(properties, StorageLevel.fromString(storageLevel))
-      )
-
-    receiverStream.
-      map(data => new Event(Map(RAW_DATA_KEY -> data.getBytes("UTF-8").asInstanceOf[java.io.Serializable])))
-
+    RabbitMQUtils.createStreamFromAQueue(ssc, "localhost", port, "test", StorageLevel.MEMORY_AND_DISK_SER_2)
+      .map(data => new Event(Map(RAW_DATA_KEY -> data.getBytes("UTF-8").asInstanceOf[java.io.Serializable])))
   }
 }
