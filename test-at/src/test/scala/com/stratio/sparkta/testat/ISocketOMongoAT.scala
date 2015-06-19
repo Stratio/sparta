1b11ed3cefcf2bc4fc4b61626847aec1c8a0e8ab
Alvaro Nistal <alvaro.nistal@gmail.com>
2015-06-19 10:23:11 +0200

    Fixed race condition in MongoDB AT

--- test-at/src/test/scala/com/stratio/sparkta/testat/ISocketOMongoAT.scala
+++ test-at/src/test/scala/com/stratio/sparkta/testat/ISocketOMongoAT.scala
@@ -30,6 +30,8 @@ class ISocketOMongoAT extends MongoEmbedDatabase with SparktaATSuite {
 
   val PolicyEndSleep = 30000
   val TestMongoPort = 60000
+  val DatabaseName = "csvtest"
+  val CollectionName = "product"
   val PathToPolicy = getClass.getClassLoader.getResource("policies/ISocket-OMongo.json").getPath
   val PathToCsv = getClass.getClassLoader.getResource("fixtures/at-data.csv").getPath
   var mongoProps: MongodProps = _
@@ -57,16 +59,20 @@ class ISocketOMongoAT extends MongoEmbedDatabase with SparktaATSuite {
     }
 
     def checkMongoData(): Unit = {
-      MongoConnection(Localhost, TestMongoPort)("csvtest")("product").find().map(dbObject => {
+      val mongoConnection = getMongoConnection()
+
+      mongoConnection.size should be(2)
+      mongoConnection.find().map(dbObject => {
         dbObject.get("id") match {
           case "producta" => {
             dbObject.get("avg_price") should be(750.0d)
             dbObject.get("sum_price") should be(6000.0d)
           }
-          case "productB" => {
+          case "productb" => {
             dbObject.get("avg_price") should be(1000.0d)
             dbObject.get("sum_price") should be(8000.0d)
           }
+          case _ => require(false)
         }
       })
     }
@@ -74,6 +80,10 @@ class ISocketOMongoAT extends MongoEmbedDatabase with SparktaATSuite {
     def checkMongoDb: Unit = {
       val mongoClientURI = MongoClientURI(s"mongodb://$Localhost:$TestMongoPort/local")
       mongoClientURI.database should be(Some("local"))
+    }
+
+    def getMongoConnection(): MongoCollection = {
+      MongoConnection(Localhost, TestMongoPort)(DatabaseName)(CollectionName)
     }
   }
 }
