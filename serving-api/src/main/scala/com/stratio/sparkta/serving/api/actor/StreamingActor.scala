42f0743a7b510f72d40948aa962752f0f7375aef
Daniel Carroza <danielcarrozasantana@gmail.com>
2015-07-09 16:34:49 +0200

    working on... a test fails

---
+++ serving-api/src/main/scala/com/stratio/sparkta/serving/api/actor/StreamingActor.scala
@@ -0,0 +1,149 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stratio.sparkta.driver.actor
+
+import akka.actor.SupervisorStrategy.Escalate
+import akka.actor._
+import akka.pattern.ask
+import akka.util.Timeout
+import com.stratio.sparkta.driver.exception.ServingApiException
+import com.stratio.sparkta.driver.models.StreamingContextStatusEnum._
+import com.stratio.sparkta.driver.models.{AggregationPoliciesModel, PolicyElementModel, StreamingContextStatus, StreamingContextStatusEnum}
+import com.stratio.sparkta.driver.service.StreamingContextService
+
+import scala.concurrent.ExecutionContext.Implicits.global
+import scala.concurrent.duration._
+import scala.util.{Failure, Success}
+
+case class CreateContext(policy: AggregationPoliciesModel)
+
+case class GetContextStatus(contextName: String)
+
+case class GetAllContextStatus()
+
+case class StopContext(contextName: String)
+
+case class DeleteContext(contextName: String)
+
+case class CreateFragment(fragment: PolicyElementModel)
+
+case class ContextActorStatus(actor: ActorRef, status: StreamingContextStatusEnum.Status, description: Option[String])
+
+class StreamingActor(streamingContextService: StreamingContextService) extends InstrumentedActor {
+
+  private var contextActors: Map[String, ContextActorStatus] = Map()
+
+  override val supervisorStrategy =
+    OneForOneStrategy() {
+      case _: ServingApiException => Escalate
+      case t =>
+        super.supervisorStrategy.decider.applyOrElse(t, (_: Any) => Escalate)
+    }
+
+  override def receive: PartialFunction[Any, Unit] = {
+    case CreateContext(policy) => doCreateContext(policy)
+    case GetContextStatus(contextName) => doGetContextStatus(contextName)
+    case DeleteContext(contextName) => doDeleteContext(contextName)
+    case GetAllContextStatus => doGetAllContextStatus()
+  }
+
+  /**
+   * Tries to create a spark streaming context with a given configuration.
+   * @param policy that contains the configuration to run.
+   */
+  private def doCreateContext(policy: AggregationPoliciesModel): Unit = {
+    val streamingContextActor = context.actorOf(
+      Props(new StreamingContextActor(policy, streamingContextService)), "context-actor-".concat(policy.name))
+    contextActors += (policy.name -> new ContextActorStatus(streamingContextActor, Initializing, None))
+
+    (streamingContextActor ? Init)(Timeout(10 minutes)).onComplete {
+      case Success(Initialized) =>
+        log.info("Context initialized with name: " + policy.name)
+        contextActors.get(policy.name) match {
+          case Some(contextActorStatus) =>
+            contextActors += (policy.name -> new ContextActorStatus(contextActorStatus.actor, Initialized, None))
+        }
+      case Success(InitError(e: ServingApiException)) =>
+        log.warn("Configuration error! in context with name: " + policy.name)
+        contextActors.get(policy.name) match {
+          case Some(contextActorStatus) =>
+            contextActors += (policy.name ->
+              new ContextActorStatus(contextActorStatus.actor,ConfigurationError, Option(e.getMessage)))
+            contextActorStatus.actor ! PoisonPill
+        }
+      case Success(InitError(e)) =>
+        log.error("Error initializing StreamingContext with name: " + policy.name, e)
+        contextActors.get(policy.name) match {
+          case Some(contextActorStatus) =>
+            contextActors += (policy.name ->
+              new ContextActorStatus(contextActorStatus.actor, Error, Option(e.getMessage)))
+            contextActorStatus.actor ! PoisonPill
+        }
+      case Failure(e: Exception) =>
+        log.error("Akka error initializing StreamingContext with name: " + policy.name, e)
+        contextActors.get(policy.name) match {
+          case Some(contextActorStatus) =>
+            contextActors += (policy.name ->
+              new ContextActorStatus(contextActorStatus.actor, Error, Option(e.getMessage)))
+            contextActorStatus.actor ! PoisonPill
+        }
+      case x =>
+        log.warn("Unexpected message received by streamingContextActor: " + x)
+    }
+  }
+
+  /**
+   * If a context with a specific contextName exists, it will retrieve information about it.
+   * @param contextName of the context to obtain information.
+   */
+  private def doGetContextStatus(contextName: String): Unit = {
+    contextActors.get(contextName) match {
+      case Some(contextActorStatus) =>
+        sender ! new StreamingContextStatus(contextName, contextActorStatus.status, contextActorStatus.description)
+      case None =>
+        throw new ServingApiException("Context with name " + contextName + " does not exists.")
+    }
+  }
+
+  /**
+   * If a context with a specific contextName exists, it will try to delete it.
+   * @param contextName of the context to delete.
+   */
+  private def doDeleteContext(contextName: String): Unit = {
+    contextActors.get(contextName) match {
+      case Some(contextActorStatus) =>
+        contextActorStatus.actor ! PoisonPill
+        contextActors -= contextName
+        sender ! new StreamingContextStatus(contextName, Removed, None)
+      case None =>
+        throw new ServingApiException("Context with name " + contextName + " does not exists.")
+    }
+  }
+
+  /**
+   * Retrieves information of all running contexts.
+   */
+  private def doGetAllContextStatus(): Unit = {
+    sender ! contextActors.map(cas =>
+      new StreamingContextStatus(cas._1, cas._2.status, cas._2.description)).toSeq
+  }
+
+  override def postStop(): Unit = {
+    contextActors.values.foreach(_.actor ! PoisonPill)
+    super.postStop()
+  }
+}
---
+++ serving-api/src/test/scala/com/stratio/sparkta/serving/api/test/helpers/PolicyHelperSpec.scala
@@ -0,0 +1,79 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stratio.sparkta.driver.test.helpers
+
+import com.stratio.sparkta.driver.models._
+import com.stratio.sparkta.driver.helpers.PolicyHelper
+import org.junit.runner.RunWith
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+
+/**
+ * Tests over policy operations.
+ * @author anistal
+ */
+
+@RunWith(classOf[JUnitRunner])
+class PolicyHelperSpec extends FeatureSpec with GivenWhenThen with Matchers {
+
+  feature("A policy that contains fragments must parse these fragments and join them to input/outputs depending of " +
+    "its type") {
+    Given("a policy with an input, an output and a fragment with an input")
+    val checkpointInterval = 10000
+    val checkpointAvailable = 60000
+    val checkpointGranularity = "minute"
+    val checkpointDir = "checkpoint"
+    val checkpointDto =
+      new CheckpointModel(checkpointDir, "", checkpointGranularity, checkpointInterval, checkpointAvailable)
+
+    val ap = new AggregationPoliciesModel(
+      "policy-test",
+      sparkStreamingWindow = 2000,
+      new RawDataModel(),
+      transformations = Seq(),
+      cubes = Seq(),
+      inputs = Seq(
+        PolicyElementModel("input1", "input", Map())),
+      outputs = Seq(
+        PolicyElementModel("output1", "output", Map())),
+      fragments = Seq(
+        FragmentElementModel(
+          name = "fragment1",
+          fragmentType = "input",
+          element = PolicyElementModel("inputF", "input", Map())),
+        FragmentElementModel(
+          name = "fragment1",
+          fragmentType = "output",
+          element = PolicyElementModel("outputF", "output", Map()))),
+      checkpointing = checkpointDto
+    )
+
+    When("the helper parse these fragments")
+    val result = PolicyHelper.parseFragments(ap)
+
+    Then("inputs/outputs must have the existing input/outputs and the parsed input fragment")
+    result.inputs.toSet should equal(Seq(
+      PolicyElementModel("input1", "input", Map()),
+      PolicyElementModel("inputF", "input", Map())).toSet
+    )
+
+    result.outputs.toSet should equal(Seq(
+      PolicyElementModel("output1", "output", Map()),
+      PolicyElementModel("outputF", "output", Map())).toSet
+    )
+  }
+}
