1bff3f9b32cfd65987557086762487e97f9e159f
Mario Arbesu <marbesu@stratio.com>
2015-08-21 12:29:43 +0200

    feature/inputs-v1: duplicate inputs modal - validate form $ validate name

--- web/src/scripts/services/api/api_fragment_service.js
+++ web/src/scripts/services/api/api_fragment_service.js
@@ -8,21 +8,17 @@
     ApiFragmentDataService.$inject = ['$resource'];
 
     function ApiFragmentDataService($resource) {
-        return $resource(
-/*
-             API_URL+'dataview/:id',
-            { id: '@id' },
-*/
-            {
+        var vm = this;
 
-/*
-                forcedelete: { method: 'DELETE', url: API_URL+'dataview/:id/force' },
-                query: { method: 'GET', url: API_URL+'dataviews', isArray: false },
-                save: { method: 'POST', url: API_URL+'dataviews' },
-                update: { method:'PUT' },
-                widgets: { method: 'GET', url: API_URL+'dataview/:id/widgets' }
-*/
-            }
-        );
+        vm.GetFragmentByName = GetFragmentByName;
+
+        /////////////////////////////////
+
+        function GetFragmentByName() {
+            return $resource('/fragment/:type/:name', {type:'@type', name:'@name'},
+            {
+                'get'   : {method:'GET'}
+            });
+        };
     }
 })();
