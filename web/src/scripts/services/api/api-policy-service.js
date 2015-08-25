daf2b2cadb13faf5ff0b1360fbc5d920d7cbe513
alexrchies <alexrchies@gmail.com>
2015-08-25 12:10:05 +0200

    refactor(policy factory): move functions to policy factory

--- web/src/scripts/services/api/api-policy-service.js
+++ web/src/scripts/services/api/api-policy-service.js
@@ -11,9 +11,6 @@
         var vm = this;
 
         vm.GetPolicyByFragmentName = GetPolicyByFragmentName;
-        vm.DeleteFragment = DeleteFragment;
-        vm.CreateFragment = CreateFragment;
-        vm.UpdateFragment = UpdateFragment;
 
         /////////////////////////////////
 
@@ -21,27 +18,6 @@
             return $resource('/policy/fragment/:type/:name', {type:'@type', name:'@name'},
             {
                 'get': {method:'GET', isArray:true}
-            });
-        };
-
-        function DeleteFragment() {
-            return $resource('/fragment/:type/:name', {type:'@type', name:'@name'},
-            {
-                'delete': {method:'DELETE'}
-            });
-        };
-
-        function CreateFragment() {
-            return $resource('/fragment/', {},
-            {
-                'create': {method:'POST'}
-            });
-        };
-
-        function UpdateFragment() {
-            return $resource('/fragment/', {},
-            {
-               'update': {method:'PUT'}
             });
         };
     };
--- web/src/scripts/services/fragment-data-service.js
+++ web/src/scripts/services/fragment-data-service.js
@@ -5,27 +5,24 @@
        .module('webApp')
        .factory('FragmentDataService', FragmentDataService);
 
-   FragmentDataService.$inject = ['ApiPolicyService', 'ApiFragmentDataService'];
+   FragmentDataService.$inject = ['ApiFragmentService'];
 
-   function FragmentDataService(ApiPolicyService, ApiFragmentDataService) {
+   function FragmentDataService(ApiFragmentService) {
        return {
-               GetPolicyByFragmentName: function(fragmentType, fragmentName) {
-                   return ApiPolicyService.GetPolicyByFragmentName().get({'type': fragmentType ,'name': fragmentName}).$promise;
-               },
                DeleteFragment: function(fragmentType, fragmentName) {
-                   return ApiPolicyService.DeleteFragment().delete({'type': fragmentType ,'name': fragmentName}).$promise;
+                   return ApiFragmentService.DeleteFragment().delete({'type': fragmentType ,'name': fragmentName}).$promise;
                },
-               InsertFragment: function(newFragmentData) {
-                   return ApiPolicyService.CreateFragment().create(newFragmentData).$promise;
+               CreateFragment: function(newFragmentData) {
+                   return ApiFragmentService.CreateFragment().create(newFragmentData).$promise;
                },
                UpdateFragment: function(updatedFragmentData) {
-                   return ApiPolicyService.UpdateFragment().update(updatedFragmentData).$promise;
+                   return ApiFragmentService.UpdateFragment().update(updatedFragmentData).$promise;
                },
                GetFragmentByName: function(fragmentType, fragmentName) {
-                  return ApiFragmentDataService.GetFragmentByName().get({'type': fragmentType ,'name': fragmentName}).$promise;
+                  return ApiFragmentService.GetFragmentByName().get({'type': fragmentType ,'name': fragmentName}).$promise;
                },
                GetFragments: function(fragmentType) {
-                  return ApiFragmentDataService.GetFragments().get({'type': fragmentType}).$promise;
+                  return ApiFragmentService.GetFragments().get({'type': fragmentType}).$promise;
                }
            };
    };
