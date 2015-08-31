c566a5a582e117355c7d54c0c1c40d6c1a295375
Mario Arbesu <marbesu@stratio.com>
2015-08-31 11:59:55 +0200

    refactor: create input controllers

---
+++ web/src/scripts/controllers/inputs/delete-input.js
@@ -0,0 +1,26 @@
+(function() {
+  'use strict';
+
+    /*DELETE INPUT MODALS CONTROLLER */
+    angular
+        .module('webApp')
+        .controller('DeleteFragmentModalCtrl', DeleteFragmentModalCtrl);
+
+    DeleteFragmentModalCtrl.$inject = ['$scope', '$modalInstance', 'item'];
+
+    function DeleteFragmentModalCtrl($scope, $modalInstance, item) {
+        console.log('*********Modal');
+        console.log(item);
+
+        $scope.inputs = item;
+
+        $scope.ok = function () {
+            $modalInstance.close($scope.inputs);
+        };
+
+        $scope.cancel = function () {
+            $modalInstance.dismiss('cancel');
+        };
+    };
+
+})();
