ea61d3e93fd87b292155f87571cd0945f74b78b8
Eva Maria <emgaitan@stratio.com>
2015-09-03 11:10:01 +0200

    refactor(policy controller): Remove unused function

--- web/src/scripts/controllers/policies.js
+++ web/src/scripts/controllers/policies.js
@@ -1,43 +1,36 @@
-(function() {
-    'use strict';
+(function () {
+  'use strict';
 
-    angular
-        .module('webApp')
-        .controller('PoliciesCtrl', PoliciesCtrl);
+  angular
+    .module('webApp')
+    .controller('PoliciesCtrl', PoliciesCtrl);
 
-    PoliciesCtrl.$inject = ['PolicyFactory'];
+  PoliciesCtrl.$inject = ['PolicyFactory'];
 
-    function PoliciesCtrl(PolicyFactory) {
-        /*jshint validthis: true*/
-       var vm = this;
+  function PoliciesCtrl(PolicyFactory) {
+    /*jshint validthis: true*/
+    var vm = this;
 
-       vm.policiesData = {};
-       vm.policiesData.list = [];
-vm.createPolicy = createPolicy();
-       init();
+    vm.policiesData = {};
+    vm.policiesData.list = [];
+    init();
 
-        /////////////////////////////////
+    /////////////////////////////////
 
-        function init() {
-          getPolicies();
-        };
-
-        function getPolicies() {
-            var policiesList = PolicyFactory.GetAllpolicies();
-
-            policiesList.then(function (result) {
-                console.log('--> Getting policies');
-                console.log('> Getting list of policies');
-                console.log(result);
-                vm.policiesData.list = result;
-            });
-        };
-
-
-       function createPolicy(){
-
-
-       }
-
+    function init() {
+      getPolicies();
     };
+
+    function getPolicies() {
+      var policiesList = PolicyFactory.GetAllpolicies();
+
+      policiesList.then(function (result) {
+        console.log('--> Getting policies');
+        console.log('> Getting list of policies');
+        console.log(result);
+        vm.policiesData.list = result;
+      });
+    };
+
+  };
 })();
