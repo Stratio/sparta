f4d4cb85e43e9466598fa067ca899a2f2804dad4
Eva Maria <emgaitan@stratio.com>
2015-09-04 11:17:12 +0200

    refactor(new policy cotnroller): Rename controller to wizard controller

--- web/src/scripts/controllers/policies/new-policy.js
+++ web/src/scripts/controllers/policies/new-policy.js
@@ -7,10 +7,15 @@ angular
   .controller('NewPolicyCtrl', NewPolicyCtrl);
 
 NewPolicyCtrl.$inject = ['PolicyStaticDataService', '$q'];
-function NewPolicyCtrl(PolicyStaticDataService,  $q) {
+function NewPolicyCtrl(PolicyStaticDataService) {
   var vm = this;
 
   vm.steps = PolicyStaticDataService.steps;
+  vm.currentStep = 0;
+  vm.continueToNextStep = continueToNextStep;
 
+  function continueToNextStep(){
+    vm.currentStep++;
+  }
 };
 })();
