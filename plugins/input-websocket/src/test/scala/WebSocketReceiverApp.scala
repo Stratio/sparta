ab9a9f63fb2963593211f267ee3ccbb24d943bbf
arinconstrio <arincon@stratio.com>
2015-09-09 16:10:41 +0200

    replace spec to app

--- plugins/input-websocket/src/test/scala/WebSocketReceiverApp.scala
+++ plugins/input-websocket/src/test/scala/WebSocketReceiverApp.scala
@@ -6,7 +6,7 @@ import org.apache.spark.streaming.{Seconds, StreamingContext}
 /**
  * This isn't a test, is a little spark streaming program to check the receiver
  */
-object WebSocketReceiverSpec extends App {
+object WebSocketReceiverApp extends App {
 
   val conf = new SparkConf().setAppName("websocketTest").setMaster("local[2]")
   val ssc = new StreamingContext(conf, Seconds(10))
