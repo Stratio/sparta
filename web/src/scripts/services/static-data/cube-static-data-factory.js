a44c1ab271fc18838034d461e3f49559b723d500
Eva Maria <emgaitan@stratio.com>
2015-09-09 18:54:10 +0200

    feature(policy sent): Add api call to send the created policy

--- web/src/scripts/services/static-data/cube-static-data-factory.js
+++ web/src/scripts/services/static-data/cube-static-data-factory.js
@@ -10,6 +10,9 @@
     return {
       GetFunctions: function () {
         return ["Accumulator", "Avg", "Count", "Entity count", "First value", "Full text", "Last value", "Max", "Median", "Min", "Mode", "Range", "Std_Dev", "Sum", "Variance", "Total entity count"]
+      },
+      getGranularityOptions: function () {
+        return ["seconds", "5 seconds", "10 seconds", "15 seconds", "minute", "hour", "day", "month", "year"]
       }
     }
   }
