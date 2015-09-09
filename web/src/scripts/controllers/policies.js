f5e1627030cd8b7bb0c4fa5e8ee2050ad222059b
Mario Arbesu <marbesu@stratio.com>
2015-09-09 13:40:19 +0200

    faeture(policy): delete policies

--- web/src/scripts/controllers/policies.js
+++ web/src/scripts/controllers/policies.js
@@ -5,14 +5,15 @@
     .module('webApp')
     .controller('PoliciesCtrl', PoliciesCtrl);
 
-  PoliciesCtrl.$inject = ['PolicyFactory'];
+  PoliciesCtrl.$inject = ['PolicyFactory', '$modal'];
 
-  function PoliciesCtrl(PolicyFactory) {
+  function PoliciesCtrl(PolicyFactory, $modal) {
     /*jshint validthis: true*/
     var vm = this;
 
     vm.policiesData = {};
     vm.policiesData.list = [];
+    vm.deletePolicy = deletePolicy;
     init();
 
     /////////////////////////////////
@@ -29,6 +30,39 @@
         console.log('> Getting list of policies');
         console.log(result);
         vm.policiesData.list = result;
+      });
+    };
+
+    function deletePolicy(policyId, index) {
+      console.log('--> Deleting policy');
+      var policyToDelete =
+      {
+        'id': policyId,
+        'index': index
+      };
+      deletePolicyConfirm('lg', policyToDelete);
+    };
+
+    function deletePolicyConfirm(size, policy) {
+
+      var modalInstance = $modal.open({
+        animation: true,
+        templateUrl: 'templates/policies/st-delete-policy-modal.tpl.html',
+        controller: 'DeletePolicyModalCtrl as vm',
+        size: size,
+        resolve: {
+            item: function () {
+                return policy;
+            }
+        }
+      });
+
+      modalInstance.result.then(function (selectedPolicy) {
+        vm.policiesData.list.splice(selectedPolicy.index, 1);
+        console.log(vm.policiesData.list);
+
+      },function () {
+        console.log('Modal dismissed at: ' + new Date())
       });
     };
 
