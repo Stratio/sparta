b84b736a786bfa50682cabc30b42578a186730c2
arinconstrio <arincon@stratio.com>
2015-09-10 15:28:27 +0200

    move fragments an fragments policies to their folder

--- examples/scripts/loadExampleConfiguration_Cassandra.sh
+++ examples/scripts/loadExampleConfiguration_Cassandra.sh
@@ -1,10 +1,10 @@
 #!/usr/bin/env bash
 
-INPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/WebSocketFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
+INPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/fragments/WebSocketFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
 echo $INPUT
-OUTPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/CassandraFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
+OUTPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/fragments/CassandraFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
 echo $OUTPUT
-cat ../policies/IWebSocket-OCassandra.json|sed -e "s/_input_id_/$INPUT/g"|sed -e "s/_output_id_/$OUTPUT/g" >temp.json
+cat ../policies/fragments/IWebSocket-OCassandra.json|sed -e "s/_input_id_/$INPUT/g"|sed -e "s/_output_id_/$OUTPUT/g" >temp.json
 
 POL=`curl -sX POST -H "Content-Type: application/json" --data @./temp.json  localhost:9090/policy |jq '.id'| sed -e "s/\"//g"`
 
--- examples/scripts/loadExampleConfiguration_Mongo.sh
+++ examples/scripts/loadExampleConfiguration_Mongo.sh
@@ -3,11 +3,11 @@
 
 #!/usr/bin/env bash
 
-INPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/WebSocketFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
+INPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/fragments/WebSocketFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
 echo $INPUT
-OUTPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/MongoFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
+OUTPUT=`curl -sX POST -H "Content-Type: application/json" --data @../policies/fragments/MongoFragment.json localhost:9090/fragment | jq '.id' | sed -e "s/\"//g"`
 echo $OUTPUT
-cat ../policies/IWebSocket-OMongo.json|sed -e "s/_input_id_/$INPUT/g"|sed -e "s/_output_id_/$OUTPUT/g" >temp.json
+cat ../policies/fragments/IWebSocket-OMongo.json|sed -e "s/_input_id_/$INPUT/g"|sed -e "s/_output_id_/$OUTPUT/g" >temp.json
 
 curl -X POST -H "Content-Type: application/json" --data @./temp.json  localhost:9090/policy
 
