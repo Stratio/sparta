58cb785bdb599875d2bc8ec7cb2ac903b0d5d3d2
Brezyll <germanschiavon@gmail.com>
2015-09-16 11:30:33 +0200

    Fixed bug when the policy had blank spaces

--- serving-api/src/main/scala/com/stratio/sparkta/serving/api/actor/SparkStreamingContextActor.scala
+++ serving-api/src/main/scala/com/stratio/sparkta/serving/api/actor/SparkStreamingContextActor.scala
@@ -94,6 +94,7 @@ class SparkStreamingContextActor(streamingContextService: StreamingContextServic
   }
 
   private def getStreamingContextActor(policy: AggregationPoliciesModel): Option[ActorRef] = {
+    val actorName = policy.name.replace(" ","_")
     SparktaConfig.getClusterConfig match {
       case Some(clusterConfig) => {
         val zookeeperConfig = SparktaConfig.getZookeeperConfig
@@ -103,13 +104,13 @@ class SparkStreamingContextActor(streamingContextService: StreamingContextServic
         if (zookeeperConfig.isDefined && hdfsConfig.isDefined) {
           Some(context.actorOf(Props(new ClusterSparkStreamingContextActor(
             policy, streamingContextService, clusterConfig, hdfsConfig.get, zookeeperConfig.get, detailConfig)),
-            s"$SparkStreamingContextActorPrefix-${policy.name}"))
+            s"$SparkStreamingContextActorPrefix-${actorName}"))
         } else None
       }
       case None => Some(context.actorOf(
         Props(new LocalSparkStreamingContextActor(
           policy, streamingContextService, policyStatusActor)),
-        s"$SparkStreamingContextActorPrefix-${policy.name}"))
+        s"$SparkStreamingContextActorPrefix-${actorName}"))
     }
   }
 }
