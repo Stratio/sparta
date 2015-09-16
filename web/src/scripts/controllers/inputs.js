4de95bd7c7028747b5ff0864a6c9640fa9db8975
Mario Arbesu <marbesu@stratio.com>
2015-09-16 14:11:09 +0200

    feature(fragments&policies): create error messages

--- web/src/scripts/controllers/inputs.js
+++ web/src/scripts/controllers/inputs.js
@@ -23,6 +23,8 @@
        vm.getPolicyNames = getPolicyNames;
        vm.inputsData = [];
        vm.inputTypes = [];
+       vm.error = false;
+       vm.errorMessage = '';
 
        init();
 
@@ -36,10 +38,13 @@
           var inputList = FragmentFactory.GetFragments('input');
 
           inputList.then(function (result) {
+            vm.error = false;
             vm.inputsData = result;
             vm.getInputTypes(result);
 
           },function (error) {
+            vm.error = true
+            vm.errorMessage = "_INPUT_ERROR_" + error.data.i18nCode + "_";;
             console.log('There was an error while loading the inputs flist!');
             console.log(error);
           });
