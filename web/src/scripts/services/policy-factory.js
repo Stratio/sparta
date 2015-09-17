5215910178b080dc102dd99d49cd4507106302b5
Eva Maria <emgaitan@stratio.com>
2015-09-17 18:11:25 +0200

    MERGE

--- web/src/scripts/services/policy-factory.js
+++ web/src/scripts/services/policy-factory.js
@@ -7,32 +7,35 @@
 
   PolicyFactory.$inject = ['ApiPolicyService'];
 
-   function PolicyFactory(ApiPolicyService) {
-       return {
-               getPolicyByFragmentId: function(fragmentType, fragmentId) {
-                  return ApiPolicyService.getPolicyByFragmentId().get({'type': fragmentType ,'id': fragmentId}).$promise;
-               },
-               getAllPolicies: function() {
-                  return ApiPolicyService.getAllpolicies().get().$promise;
-               },
-               createPolicy: function(newPolicyData) {
-                  return ApiPolicyService.createPolicy().create(newPolicyData).$promise;
-               },
-               deletePolicy: function(policyid) {
-                  return ApiPolicyService.deletePolicy().delete({'id': policyid}).$promise;
-               },
-               runPolicy: function(policyid) {
-                  return ApiPolicyService.runPolicy().get({'id': policyid}).$promise;
-               },
-               getFakePolicy: function() {
-                  return ApiPolicyService.getFakePolicy().get().$promise;
-               },
-         StopPolicy: function(policy) {
-           return ApiPolicyService.StopPolicy().update(policy).$promise;
-         },
-         savePolicy: function (policyData) {
-           return ApiPolicyService.savePolicy().put(policyData).$promise;
-         }
-           };
-   };
+  function PolicyFactory(ApiPolicyService) {
+    return {
+      getPolicyById: function (policyId) {
+        return ApiPolicyService.getPolicyById().get({'id': policyId}).$promise;
+      },
+      getPolicyByFragmentId: function (fragmentType, fragmentId) {
+        return ApiPolicyService.getPolicyByFragmentId().get({'type': fragmentType, 'id': fragmentId}).$promise;
+      },
+      getAllPolicies: function () {
+        return ApiPolicyService.getAllpolicies().get().$promise;
+      },
+      createPolicy: function (newPolicyData) {
+        return ApiPolicyService.createPolicy().create(newPolicyData).$promise;
+      },
+      deletePolicy: function (policyid) {
+        return ApiPolicyService.deletePolicy().delete({'id': policyid}).$promise;
+      },
+      runPolicy: function (policyid) {
+        return ApiPolicyService.runPolicy().get({'id': policyid}).$promise;
+      },
+      getFakePolicy: function () {
+        return ApiPolicyService.getFakePolicy().get().$promise;
+      },
+      StopPolicy: function (policy) {
+        return ApiPolicyService.StopPolicy().update(policy).$promise;
+      },
+      savePolicy: function (policyData) {
+        return ApiPolicyService.savePolicy().put(policyData).$promise;
+      }
+    };
+  };
 })();
