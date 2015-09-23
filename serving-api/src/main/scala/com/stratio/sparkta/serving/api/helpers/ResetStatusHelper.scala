910bc2dc5e2b5d041dde4590d456f0f21f73b249
arinconstrio <arincon@stratio.com>
2015-09-23 18:39:50 +0200

    reset policy status to not started if sparkta running in local env

---
+++ serving-api/src/main/scala/com/stratio/sparkta/serving/api/helpers/ResetStatusHelper.scala
@@ -0,0 +1,43 @@
+package com.stratio.sparkta.serving.api.helpers
+
+
+import com.stratio.sparkta.serving.api.Sparkta._
+import com.stratio.sparkta.serving.core.models.{SparktaSerializer, PolicyStatusModel}
+import com.stratio.sparkta.serving.core.policy.status.PolicyStatusEnum
+import com.stratio.sparkta.serving.core.{SparktaConfig, AppConstant, CuratorFactoryHolder}
+import org.json4s.jackson.Serialization._
+
+import scala.collection.JavaConversions
+import scala.util.{Failure, Success, Try}
+
+/**
+ * Created by arincon on 23/09/15.
+ */
+object ResetStatusHelper extends SparktaSerializer {
+
+  def ResetStatuses {
+    Try {
+      if (SparktaConfig.getClusterConfig.isEmpty) {
+        val curator = CuratorFactoryHolder.getInstance()
+        val contextPath = s"${AppConstant.ContextPath}"
+        val children = curator.getChildren.forPath(contextPath)
+        val statuses = JavaConversions.asScalaBuffer(children).toList.map(element =>
+          read[PolicyStatusModel](new String(curator.getData.forPath(
+            s"${AppConstant.ContextPath}/$element")))).toSeq
+        statuses.foreach(p => update(PolicyStatusModel(p.id, PolicyStatusEnum.NotStarted)))
+      }
+      def update(policyStatus: PolicyStatusModel): Unit = {
+        val curator = CuratorFactoryHolder.getInstance()
+        val statusPath = s"${AppConstant.ContextPath}/${policyStatus.id}"
+        val ips =
+          read[PolicyStatusModel](new String(curator.getData.forPath(statusPath)))
+        log.info(s">> Updating context ${policyStatus.id} : <${ips.status}> to <${policyStatus.status}>")
+        curator.setData().forPath(statusPath, write(policyStatus).getBytes)
+      }
+    } match {
+      case Failure(ex: Exception) => throw ex
+
+    }
+  }
+
+}
