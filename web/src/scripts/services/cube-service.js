0d640924dc34fc5f3be2b770621852ddc21886fb
Eva Maria <emgaitan@stratio.com>
2015-09-29 17:31:01 +0200

    refactor(cube management): Add refactor to cube accordion and cube controllers

--- web/src/scripts/services/cube-service.js
+++ web/src/scripts/services/cube-service.js
@@ -5,27 +5,75 @@
     .module('webApp')
     .service('CubeService', CubeService);
 
-  CubeService.$inject = ['PolicyModelFactory', 'ModalService', 'AccordionStatusService', 'CubeModelFactory', '$q'];
+  CubeService.$inject = ['UtilsService', 'ModalService'];
 
-  function CubeService(PolicyModelFactory, ModalService, AccordionStatusService, CubeModelFactory, $q) {
+  function CubeService(UtilsService, ModalService) {
     var vm = this;
-    var createdCubes = null;
-
     vm.findCubesUsingOutputs = findCubesUsingOutputs;
+    vm.isValidCube = isValidCube;
     vm.areValidCubes = areValidCubes;
     vm.showConfirmRemoveCube = showConfirmRemoveCube;
-    vm.addCube = addCube;
-    vm.saveCube = saveCube;
-    vm.removeCube = removeCube;
-    vm.isNewCube = isNewCube;
-    vm.getCreatedCubes = getCreatedCubes;
-    vm.resetCreatedCubes = resetCreatedCubes;
-    init();
 
-    function init() {
-      vm.policy = PolicyModelFactory.getCurrentPolicy();
+    function findCubesUsingOutputs(cubes, outputs) {
+      var cubeNames = [];
+      var cubePositions = [];
+      var currentCube = null;
+      var found = false;
+      for (var i = 0; i < cubes.length; ++i) {
+        found = false;
+        currentCube = cubes[i];
+        found = findDimensionUsingOutputs(currentCube, outputs);
+        if (found) {
+          cubeNames.push(currentCube.name);
+          cubePositions.push(i);
+        }
+      }
+      return {names: cubeNames, positions: cubePositions};
+    }
 
-      createdCubes = vm.policy.cubes.length;
+    function findDimensionUsingOutputs(cube, outputs) {
+      var found = false;
+      var currentDimension = null;
+      var i = 0;
+      while (!found && i < cube.dimensions.length) {
+        currentDimension = cube.dimensions[i];
+        if (outputs.indexOf(currentDimension.field) != -1) {
+          found = true;
+        } else {
+          ++i;
+        }
+      }
+      return found;
+    }
+
+    function isValidCube(cube, cubes, cubePosition) {
+      return cube.name !== "" && cube.checkpointConfig.timeDimension !== "" && cube.checkpointConfig.interval !== null
+        && cube.checkpointConfig.timeAvailability !== null && cube.checkpointConfig.granularity !== ""
+        && cube.dimensions.length > 0 && cube.operators.length > 0 && !nameExists(cube, cubes, cubePosition);
+    }
+
+    function nameExists(cube, cubes, cubePosition) {
+      var position = UtilsService.findElementInJSONArray(cubes, cube, "name");
+      return position !== -1 && (position != cubePosition);
+    }
+
+    function areValidCubes(cubes) {
+      var valid = true;
+      var i = 0;
+      var currentCube = null;
+      if (cubes) {
+        while (valid && i < cubes.length) {
+          currentCube = cubes[i];
+          if (!isValidCube(currentCube, cubes, i)) {
+            valid = false;
+          } else {
+            ++i;
+          }
+        }
+      } else {
+        valid = false;
+      }
+      return valid;
     }
 
     function showConfirmRemoveCube() {
@@ -49,108 +97,6 @@
         defer.reject();
       });
       return defer.promise;
-    }
-
-    function findCubesUsingOutputs(outputs) {
-      var cubeNames = [];
-      var cubePositions = [];
-      var cubes = vm.policy.cubes;
-      if (cubes && outputs) {
-        var currentCube = null;
-        var found = false;
-        for (var i = 0; i < cubes.length; ++i) {
-          found = false;
-          currentCube = cubes[i];
-          found = findDimensionUsingOutputs(currentCube, outputs);
-          if (found) {
-            cubeNames.push(currentCube.name);
-            cubePositions.push(i);
-          }
-        }
-      }
-      return {names: cubeNames, positions: cubePositions};
-    }
-
-    function findDimensionUsingOutputs(cube, outputs) {
-      var found = false;
-      var currentDimension = null;
-      var i = 0;
-      while (!found && i < cube.dimensions.length) {
-        currentDimension = cube.dimensions[i];
-        if (outputs.indexOf(currentDimension.field) != -1) {
-          found = true;
-        } else {
-          ++i;
-        }
-      }
-      return found;
-    }
-
-    function areValidCubes() {
-      var valid = true;
-      var i = 0;
-      var currentCube = null;
-      if (vm.policy.cubes.length > 0) {
-        while (valid && i < vm.policy.cubes.length) {
-          currentCube = vm.policy.cubes[i];
-
-          if (!CubeModelFactory.isValidCube(currentCube, vm.policy.cubes, i)) {
-            valid = false;
-          } else {
-            ++i;
-          }
-        }
-      } else {
-        valid = false;
-      }
-      return valid;
-    }
-
-
-    function addCube() {
-      var newCube = angular.copy(CubeModelFactory.getCube());
-      if (CubeModelFactory.isValidCube(newCube, vm.policy.cubes, CubeModelFactory.getContext().position)) {
-        vm.policy.cubes.push(newCube);
-        createdCubes++;
-        AccordionStatusService.resetAccordionStatus(vm.policy.cubes.length);
-      } else {
-        CubeModelFactory.setError("_GENERIC_FORM_ERROR_");
-      }
-    }
-
-    function saveCube() {
-      var cube = angular.copy(CubeModelFactory.getCube());
-      if (CubeModelFactory.isValidCube(cube, vm.policy.cubes, CubeModelFactory.getContext().position)) {
-        vm.policy.cubes[CubeModelFactory.getContext().position] = cube;
-        CubeModelFactory.setError("");
-      } else {
-        CubeModelFactory.setError("_GENERIC_FORM_ERROR_");
-      }
-    }
-
-    function removeCube() {
-      var defer = $q.defer();
-      var cubePosition = CubeModelFactory.getContext().position;
-      showConfirmRemoveCube().then(function () {
-        vm.policy.cubes.splice(cubePosition, 1);
-        AccordionStatusService.resetAccordionStatus(vm.policy.cubes.length);
-        defer.resolve();
-      }, function () {
-        defer.reject()
-      });
-      return defer.promise;
-    }
-
-    function isNewCube(index) {
-      return index == vm.policy.cubes.length;
-    }
-
-    function getCreatedCubes() {
-      return createdCubes;
-    }
-
-    function resetCreatedCubes() {
-      createdCubes = vm.policy.cubes.length;
     }
   }
 })();
