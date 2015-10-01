d0c666e3aa2eb9444548cc013ae375e8c2799c36
arinconstrio <arincon@stratio.com>
2015-10-01 16:04:25 +0200

    test coverage over driver module

---
+++ driver/src/test/scala/com/stratio/sparkta/driver/test/util/HdfsUtilsSpec.scala
@@ -0,0 +1,80 @@
+/**
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ * http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+package com.stratio.sparkta.driver.test.util
+
+import java.io.{FileNotFoundException, InputStream}
+
+import com.stratio.sparkta.driver.util.HdfsUtils
+import org.apache.hadoop.fs.{FileSystem, _}
+import org.apache.hadoop.security.UserGroupInformation
+import org.junit.runner.RunWith
+import org.mockito.Mockito._
+import org.scalatest._
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.mock.MockitoSugar
+
+import scala.util.{Failure, Try}
+
+
+@RunWith(classOf[JUnitRunner])
+class HdfsUtilsSpec extends FlatSpec with ShouldMatchers with MockitoSugar {
+
+  val ugi: UserGroupInformation = HdfsUtils.ugi("test_user")
+
+  val fileSystem: FileSystem = mock[FileSystem]
+
+  val utils = new HdfsUtils(ugi, fileSystem)
+
+  "hdfs utils" should "getfiles from a path" in {
+    val expected = Array(mock[FileStatus])
+    when(fileSystem.listStatus(new Path("myTestPath"))).thenReturn(expected)
+
+    val result = utils.getFiles("myTestPath")
+
+    result should be(expected)
+
+  }
+  it should "return single file as inputStream" in {
+    val expected: InputStream = mock[FSDataInputStream]
+    when(fileSystem.open(new Path("testFile"))).thenReturn(expected.asInstanceOf[FSDataInputStream])
+
+    val result: InputStream = utils.getFile("testFile")
+
+    result should be(expected)
+  }
+
+
+  it should "write" in {
+    val result = Try(utils.write("from", "to", true)) match {
+      case Failure(ex: Throwable) => ex
+    }
+    result.isInstanceOf[FileNotFoundException] should be(true)
+  }
+  it should "write without override" in {
+    val result = Try(utils.write("from", "to", false)) match {
+      case Failure(ex: Throwable) => ex
+    }
+    result.isInstanceOf[FileNotFoundException] should be(true)
+  }
+  "configuration" should "return a fileSystem" in {
+    val result = Try(HdfsUtils.hdfsConfiguration(Some("miConfDir"))) match {
+      case Failure(ex: Throwable) => ex
+    }
+    result.isInstanceOf[FileNotFoundException] should be(true)
+  }
+
+}
+
