8d03b6207d904a88ac1d563874e11ca01fd54fe1
Eva Maria <emgaitan@stratio.com>
2015-10-08 17:58:04 +0200

    feature(c-json-text-directive): Add unit tests to c-json-text directive

--- web/src/scripts/directives/c-json-text-directive.js
+++ web/src/scripts/directives/c-json-text-directive.js
@@ -5,38 +5,31 @@
 
   angular
     .module('webApp')
-    .directive('jsonText', function() {
+    .directive('jsonText', function () {
       return {
         restrict: 'A', // only activate on element attribute
         require: 'ngModel', // get a hold of NgModelController
-        link: function(scope, element, attrs, ngModelCtrl) {
-
+        link: function (scope, element, attrs, ngModelCtrl) {
           var lastValid;
-
           // push() if faster than unshift(), and avail. in IE8 and earlier (unshift isn't)
           ngModelCtrl.$parsers.push(fromUser);
           ngModelCtrl.$formatters.push(toUser);
 
           // clear any invalid changes on blur
-          element.bind('blur', function() {
-            try{
-              var newValue = angular.fromJson(element[0].value);
+          element.bind('blur', function () {
+            try {
+               var newValue = angular.fromJson(element[0].value);
               lastValid = lastValid || newValue;
 
               ngModelCtrl.$setViewValue(toUser(newValue));
-
-              // TODO avoid this causing the focus of the input to be lost..
               ngModelCtrl.$render();
             }
-           catch (e)
-          {
-          }
-
+            catch (e) {
+            }
           });
 
           function fromUser(text) {
             // Beware: trim() is not available in old browsers
-
             if (!text || text.trim() === '') {
               return {};
             } else {
