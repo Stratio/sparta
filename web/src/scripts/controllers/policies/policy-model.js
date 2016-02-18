b405c8c302929afb5c4c3d45dc105ef0c1c4adb1
Eva Maria <emgaitan@stratio.com>
2016-02-18 10:50:21 +0100

    [SPARKTA-425][WEB]: Fix template to models

--- web/src/scripts/controllers/policies/policy-model.js
+++ web/src/scripts/controllers/policies/policy-model.js
@@ -29,7 +29,7 @@
       if (vm.model) {
         vm.modelError = ModelFactory.getError();
         vm.modelContext = ModelFactory.getContext();
-        vm.modelTypes = vm.template.types;
+        vm.modelTypes = vm.template.model.types;
         vm.configPlaceholder = vm.template.configPlaceholder;
         vm.outputPattern = vm.template.outputPattern;
         vm.outputInputPlaceholder = vm.template.outputInputPlaceholder;
@@ -42,19 +42,20 @@
     }
 
     function getDefaultConfigurations(type) {
-      var types = vm.template.types;
+      var types = vm.template.model.types;
+      var defaultConfigurations =  vm.template.model.defaultConfiguration;
       switch (type) {
         case types[0].name:
         {
-          return vm.template.morphlinesDefaultConfiguration;
+          return defaultConfigurations.morphlinesDefaultConfiguration;
         }
         case types[1].name:
         {
-          return vm.template.dateTimeDefaultConfiguration;
+          return defaultConfigurations.dateTimeDefaultConfiguration;
         }
         case types[2].name:
         {
-          return vm.template.typeDefaultConfiguration;
+          return defaultConfigurations.typeDefaultConfiguration;
         }
       }
     }
