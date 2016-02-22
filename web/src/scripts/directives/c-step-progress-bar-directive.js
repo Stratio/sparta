93a9aceb154ec6517b4cdde561e56dbc95bf6c3e
Eva Maria <emgaitan@stratio.com>
2016-02-22 13:32:51 +0100

    feature(policy wizard):Enable next step when input is added

--- web/src/scripts/directives/c-step-progress-bar-directive.js
+++ web/src/scripts/directives/c-step-progress-bar-directive.js
@@ -11,13 +11,15 @@ function stepsComponent() {
     restrict: 'E',
     scope: {
       steps: '=steps',
-      current: '=currentStep'
+      current: '=currentStep',
+      nextStepAvailable: '='
     },
     replace: 'true',
     templateUrl: 'templates/components/c-step-progress-bar.tpl.html',
     link: function(scope){
       scope.chooseStep = function (index) {
         scope.current = index;
+        scope.nextStepAvailable = false;
       };
     }
   };
