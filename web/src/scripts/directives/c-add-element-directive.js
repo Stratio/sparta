604794c06136b5e255cf88c22b7c689ab6b50feb
Mario Arbesu <marbesu@stratio.com>
2016-02-24 17:27:46 +0100

    fix: trnaformation forms

--- web/src/scripts/directives/c-add-element-directive.js
+++ web/src/scripts/directives/c-add-element-directive.js
@@ -21,7 +21,10 @@
         placeholder: "@",
         error: '=',
         elementsLength: '=',
-        submittedForm: '='
+        submittedForm: '=',
+        label: '=',
+        help: '=',
+        limit: '='
       },
       replace: "true",
       templateUrl: 'templates/components/c-add-element.tpl.html',
@@ -32,23 +35,33 @@
 
     function link(scope) {
       scope.addInput = addInput;
+      scope.errorLimit = false;
+
       function addInput(event) {
         if (scope.inputToAdd !== '' && scope.inputToAdd !== undefined && (event.keyCode == '13' || event.type === "click")) {
           var inputExists = false;
-          for (var i = 0; i < scope.model.length; i++) {
-            if (scope.model[i].name === scope.inputToAdd) {
-              inputExists = true;
+          scope.errorLimit = false;
+          if (scope.limit === 0 || scope.elementsLength < scope.limit) {
+            for (var i = 0; i < scope.model.length; i++) {
+              if (scope.model[i].name === scope.inputToAdd) {
+                inputExists = true;
+              }
             }
-          }
-          if (inputExists) {
-            scope.error = true;
+            if (inputExists) {
+              scope.submittedForm = true;
+              scope.error = true;
+            }
+            else {
+              scope.submittedForm = false;
+              scope.error = false;
+              scope.model.push({name:scope.inputToAdd});
+              scope.inputToAdd = '';
+            }
+            event.preventDefault();
           }
           else {
-            scope.error = false;
-            scope.model.push({name:scope.inputToAdd});
-            scope.inputToAdd = '';
+            scope.errorLimit = true;
           }
-          event.preventDefault();
         }
       }
     }
