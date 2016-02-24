db531b54f5c790c1e4047f7d20b370c4d6384c61
Eva Maria <emgaitan@stratio.com>
2016-02-24 18:24:58 +0100

    refactor(accordion): Add unit test to accordion status service

--- web/test/policies/wizard/service/accordion-status-service.js
+++ web/test/policies/wizard/service/accordion-status-service.js
@@ -19,14 +19,14 @@ describe('policies.wizard.service.accordion-status-service', function () {
       expect(resetAccordionStatus).toEqual(accordionStatus);
 
       length = undefined;
-      resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus),length);
+      resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), length);
 
       expect(resetAccordionStatus).toEqual(accordionStatus);
     });
 
     it("if the length param is valid, accordion status is reset with the that length more one position", function () {
       var length = 5;
-     var resetAccordionStatus= service.resetAccordionStatus(angular.copy(accordionStatus),length);
+      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), length);
 
       expect(resetAccordionStatus.length).toEqual(length + 1);
     });
@@ -34,7 +34,7 @@ describe('policies.wizard.service.accordion-status-service', function () {
     it("if the third param is introduced, that param is used to specify the position that has to be true", function () {
       var length = 5;
       var truePosition = 4;
-      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus),length, truePosition);
+      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), length, truePosition);
 
       expect(resetAccordionStatus.length).toEqual(length + 1);
       for (var i = 0; i <= length; ++i) {
@@ -47,10 +47,18 @@ describe('policies.wizard.service.accordion-status-service', function () {
     it("accordion status length is adjusted to the new length if the new length is minor than accordion status one", function () {
       var oldLength = 5;
       var newLength = 3;
-      var resetAccordionStatus =service.resetAccordionStatus(angular.copy(accordionStatus),oldLength);
-      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus),newLength);
+      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), oldLength);
+      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), newLength);
 
-      expect(resetAccordionStatus.length).toBe(newLength+1);
+      expect(resetAccordionStatus.length).toBe(newLength + 1);
+    })
+
+    it("if length is minor thant current length, last positions are removing until to have the same length", function () {
+      var newLength = 1;
+
+      var resetAccordionStatus = service.resetAccordionStatus(angular.copy(accordionStatus), newLength);
+
+      expect(resetAccordionStatus.length).toBe(newLength + 1);
     })
   })
 
