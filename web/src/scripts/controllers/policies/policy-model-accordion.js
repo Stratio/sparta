9482ef86071f088feda24141277789d8fb60fcf1
Eva Maria <emgaitan@stratio.com>
2016-02-25 09:25:02 +0100

    refactor(accordion): Add refactor to avoid repeating functionality

--- web/src/scripts/controllers/policies/policy-model-accordion.js
+++ web/src/scripts/controllers/policies/policy-model-accordion.js
@@ -6,19 +6,20 @@
     .module('webApp')
     .controller('PolicyModelAccordionCtrl', PolicyModelAccordionCtrl);
 
-  PolicyModelAccordionCtrl.$inject = ['PolicyModelFactory', 'ModelFactory', 'ModelService', '$scope'];
+  PolicyModelAccordionCtrl.$inject = ['PolicyModelFactory', 'ModelFactory', 'ModelService'];
 
-  function PolicyModelAccordionCtrl(PolicyModelFactory, ModelFactory, ModelService, $scope) {
+  function PolicyModelAccordionCtrl(PolicyModelFactory, ModelFactory, ModelService) {
     var vm = this;
 
     vm.init = init;
     vm.previousStep = previousStep;
     vm.nextStep = nextStep;
+    vm.changeOpenedElement = changeOpenedElement;
     vm.isActiveModelCreationPanel = ModelService.isActiveModelCreationPanel;
     vm.activateModelCreationPanel = ModelService.activateModelCreationPanel;
 
     vm.init();
-    
+
     function init() {
       vm.template = PolicyModelFactory.getTemplate();
       vm.policy = PolicyModelFactory.getCurrentPolicy();
@@ -47,26 +48,20 @@
       }
     }
 
-    $scope.$watchCollection(
-      "vm.modelAccordionStatus",
-      function (newAccordionStatus) {
-        if (newAccordionStatus) {
-          var selectedModelPosition = newAccordionStatus.indexOf(true);
-          if (vm.policy.transformations.length > 0 && selectedModelPosition >= 0 && selectedModelPosition < vm.policy.transformations.length) {
-            var selectedModel = vm.policy.transformations[selectedModelPosition];
-            ModelFactory.setModel(selectedModel, selectedModelPosition);
-          } else {
-            var modelNumber = vm.policy.transformations.length;
-            var order = 0;
+    function changeOpenedElement(selectedModelPosition) {
+      if (vm.policy.transformations.length > 0 && selectedModelPosition >= 0 && selectedModelPosition < vm.policy.transformations.length) {
+        var selectedModel = vm.policy.transformations[selectedModelPosition];
+        ModelFactory.setModel(selectedModel, selectedModelPosition);
+      } else {
+        var modelNumber = vm.policy.transformations.length;
+        var order = 0;
 
-            if (modelNumber > 0) {
-              order = vm.policy.transformations[modelNumber - 1].order + 1
-            }
-            ModelFactory.resetModel(vm.template.model, order, vm.policy.transformations.length);
-          }
-          ModelFactory.updateModelInputs(vm.policy.transformations);
+        if (modelNumber > 0) {
+          order = vm.policy.transformations[modelNumber - 1].order + 1
         }
+        ModelFactory.resetModel(vm.template.model, order, vm.policy.transformations.length);
       }
-    );
+      ModelFactory.updateModelInputs(vm.policy.transformations);
+    }
   }
 })();
