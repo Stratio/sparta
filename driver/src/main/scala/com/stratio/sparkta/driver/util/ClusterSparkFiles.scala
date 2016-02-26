14de02a6d4f4e191e072b132527311aa67df48a4
compae <gserranojc@gmail.com>
2016-02-26 15:33:23 +0100

    sparkta running over mesos and yarn without classpath and plugins in hdfs, now fatjar with all and user plugins are loaded in the policy

--- driver/src/main/scala/com/stratio/sparkta/driver/util/ClusterSparkFiles.scala
+++ driver/src/main/scala/com/stratio/sparkta/driver/util/ClusterSparkFiles.scala
@@ -32,17 +32,8 @@ case class ClusterSparkFiles(policy: AggregationPoliciesModel, hdfs: HdfsUtils) 
     PolicyUtils.jarsFromPolicy(policy)
       .filter(file => !file.getName.contains("driver")).map(file => {
       hdfs.write(file.getAbsolutePath, pluginsJarsPath, true)
-      s"hdfs://${hdfsConfig.getString(AppConstant.HdfsMaster)}$pluginsJarsPath${file.getName}"
-    })
-  }
-
-  def getClasspathFiles(classpathJarsPath: String): Seq[String] = {
-    JarsHelper.findJarsByPath(new File(SparktaConfig.sparktaHome, AppConstant.ClasspathJarFolder),
-      Some(".jar"), None, Some("driver"), Some(Seq("plugins", "spark", "driver", "web", "serving-api")), false)
-      .distinct.map(file => {
-      hdfs.write(file.getAbsolutePath, classpathJarsPath, true)
-      s"hdfs://${hdfsConfig.getString(AppConstant.HdfsMaster)}$classpathJarsPath${file.getName}"
-    })
+      file.getName -> s"hdfs://${hdfsConfig.getString(AppConstant.HdfsMaster)}$pluginsJarsPath${file.getName}"
+    }).toMap.values.toSeq
   }
 
   def getDriverFile(driverJarPath: String): String = {
