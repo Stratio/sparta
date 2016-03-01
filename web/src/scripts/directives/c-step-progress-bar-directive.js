834e265fe59b5d00ac12294d3fb42e62a3d677de
Eva Maria <emgaitan@stratio.com>
2016-03-01 16:42:23 +0100

    [SPARKTA-422][WEB]: Add help messages to policy wizard in step navigation bar

--- web/src/scripts/directives/c-step-progress-bar-directive.js
+++ web/src/scripts/directives/c-step-progress-bar-directive.js
@@ -20,13 +20,26 @@
 
       link: function (scope) {
         scope.visited = [];
+        scope.showHelp = true;
+        scope.hideHelp = function () {
+          scope.showHelp = false;
+        };
         scope.chooseStep = function (index) {
           if ((index == scope.current + 1 && scope.nextStepAvailable) || (index < scope.current) || scope.visited[index]) {
             scope.visited[scope.current] = true;
             scope.current = index;
             scope.nextStepAvailable = false;
           }
+          scope.showHelp = true;
         };
+
+        scope.$watchCollection(
+          "nextStepAvailable",
+          function (nextStepAvailable) {
+              if (nextStepAvailable){
+                scope.showHelp = true;
+              }
+          });
       }
     };
   }
