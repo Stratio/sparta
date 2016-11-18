be217ae3e431a0fac446b027692672aed31f6a86
Eva Maria <emgaitan@stratio.com>
2016-11-18 16:33:57 +0100

    [SPARTA-695]: Add a first version of auto calculated field component

---
+++ web/src/scripts/directives/c-auto-calculated-fields-directive.js
@@ -0,0 +1,70 @@
+/*
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+(function() {
+  'use strict';
+
+  /* Accordion directive */
+
+  angular
+      .module('webApp')
+      .directive('cAutoCalculatedFields', cAutoCalculatedFields);
+
+  cAutoCalculatedFields.$inject = ['apiConfigSettings', '$resource'];
+
+  function cAutoCalculatedFields(apiConfigSettings, $resource) {
+
+    var directive = {
+      restrict: 'E',
+      scope: {
+        model: "=",
+        form: "="
+      },
+      replace: true,
+      templateUrl: 'templates/components/c-auto-calculated-fields.tpl.html',
+      link: link
+    };
+
+    return directive;
+
+    function link(scope) {
+
+      if (!scope.model) {
+        scope.model = [];
+      }
+      scope.currentAutoCalculatedFieldType = "";
+      scope.addAutoCalculatedField = function() {
+        scope.model.push({type: scope.currentAutoCalculatedFieldType, field: {}});
+      };
+      scope.removeAutoCalculatedField = function (position){
+        scope.model.splice(position, 1);
+      };
+      
+      getAutoCalculatedFieldTemplate().get().$promise.then(function(template) {
+        scope.template = template;
+      });
+    }
+
+    function getAutoCalculatedFieldTemplate() {
+      return $resource('/data-templates/auto-calculated-field.json', {},
+          {
+            'get': {
+              method: 'GET',
+              timeout: apiConfigSettings.timeout
+            }
+          });
+    }
+  }
+})();
