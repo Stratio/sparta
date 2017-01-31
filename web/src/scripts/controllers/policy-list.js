d869a6c518df01aebb625e754a08b2eb7ce0de14
Eva Maria <emgaitan@stratio.com>
2017-01-31 11:03:41 +0100

    [SPARTA-759]: Improve policy list and add more info about their status [WIP]

--- web/src/scripts/controllers/policy-list.js
+++ web/src/scripts/controllers/policy-list.js
@@ -13,18 +13,18 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-(function () {
+(function() {
   'use strict';
 
   angular
-    .module('webApp')
-    .controller('PolicyListCtrl', PolicyListCtrl);
+      .module('webApp')
+      .controller('PolicyListCtrl', PolicyListCtrl);
 
   PolicyListCtrl.$inject = ['WizardStatusService', 'PolicyFactory', 'PolicyModelFactory', 'ModalService', '$state',
-    '$translate', '$interval', '$scope', '$q'];
+    '$translate', '$interval', '$scope', '$q', '$filter', '$uibModal'];
 
   function PolicyListCtrl(WizardStatusService, PolicyFactory, PolicyModelFactory, ModalService, $state,
-                          $translate, $interval, $scope, $q) {
+                          $translate, $interval, $scope, $q, $filter, $uibModal) {
     /*jshint validthis: true*/
     var vm = this;
 
@@ -45,7 +45,9 @@
     vm.successMessage = {type: 'success', text: '', internalTrace: ''};
     vm.clusterUI = '';
     vm.loading = true;
-
+    vm.isEllipsedStatusInfo = isEllipsedStatusInfo; 
+    vm.showErrorModal = showErrorModal;
+    
     init();
 
     /////////////////////////////////
@@ -70,12 +72,12 @@
       var controller = 'PolicyCreationModalCtrl';
       var templateUrl = "templates/modal/policy-creation-modal.tpl.html";
       var resolve = {
-        title: function () {
+        title: function() {
           return "_POLICY_._MODAL_CREATION_TITLE_";
         }
       };
       var modalInstance = ModalService.openModal(controller, templateUrl, resolve, null, 'lg');
-      return modalInstance.result.then(function () {
+      return modalInstance.result.then(function() {
         WizardStatusService.nextStep();
         $state.go('wizard.newPolicy');
       });
@@ -85,7 +87,7 @@
       vm.errorMessage.text = "";
       WizardStatusService.reset();
       if (policyStatus.toLowerCase() === 'notstarted' || policyStatus.toLowerCase() === 'failed' ||
-        policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
+          policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
         $state.go(route, {"id": policyId});
       }
       else {
@@ -95,7 +97,7 @@
 
     function deletePolicy(policyId, policyStatus, index) {
       if (policyStatus.toLowerCase() === 'notstarted' || policyStatus.toLowerCase() === 'failed' ||
-        policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
+          policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
         var policyToDelete =
         {
           'id': policyId,
@@ -110,12 +112,12 @@
 
     function runPolicy(policyId, policyStatus, policyName) {
       if (policyStatus.toLowerCase() === 'notstarted' || policyStatus.toLowerCase() === 'failed' ||
-        policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
+          policyStatus.toLowerCase() === 'stopped' || policyStatus.toLowerCase() === 'stopping') {
         var policyRunning = PolicyFactory.runPolicy(policyId);
 
-        policyRunning.then(function () {
+        policyRunning.then(function() {
           vm.successMessage.text = $translate.instant('_RUN_POLICY_OK_', {policyName: policyName});
-        }, function (error) {
+        }, function(error) {
           vm.errorMessage.text = "_ERROR_._" + error.data.i18nCode + "_";
           vm.errorMessage.internalTrace = 'Error: ' + error.data.message;
         });
@@ -136,9 +138,9 @@
 
         var policyStopping = PolicyFactory.stopPolicy(stopPolicy);
 
-        policyStopping.then(function () {
+        policyStopping.then(function() {
           vm.successMessage.text = $translate.instant('_STOP_POLICY_OK_', {policyName: policyName});
-        }, function (error) {
+        }, function(error) {
           vm.errorMessage.text = '_ERROR_._' + error.data.i18nCode + '_';
           vm.errorMessage.internalTrace = 'Error: ' + error.data.message;
         });
@@ -152,13 +154,13 @@
       var controller = 'DeletePolicyModalCtrl';
       var templateUrl = "templates/policies/st-delete-policy-modal.tpl.html";
       var resolve = {
-        item: function () {
+        item: function() {
           return policy;
         }
       };
       var modalInstance = ModalService.openModal(controller, templateUrl, resolve, '', size);
 
-      modalInstance.result.then(function (selectedPolicy) {
+      modalInstance.result.then(function(selectedPolicy) {
         vm.policiesData.splice(selectedPolicy.index, 1);
         vm.successMessage.text = '_POLICY_DELETE_OK_';
       });
@@ -166,30 +168,52 @@
 
     function updatePoliciesStatus() {
       var defer = $q.defer();
-        var policiesStatus = PolicyFactory.getPoliciesStatus();
-        policiesStatus.then(function (result) {
-          vm.clusterUI = result.resourceManagerUrl;
-          var policiesWithStatus = result.policiesStatus;
-          if (policiesWithStatus) {
-            vm.policiesData = policiesWithStatus;
-            // TODO only update status
+      var policiesStatus = PolicyFactory.getPoliciesStatus();
+      policiesStatus.then(function(result) {
+        vm.clusterUI = result.resourceManagerUrl;
+        var policiesWithStatus = result.policiesStatus;
+        if (policiesWithStatus) {
+          for (var i = 0; i < policiesWithStatus.length; i++) {
+            var policyData = $filter('filter')(vm.policiesData, {'id': policiesWithStatus[i].id}, true)[0];
+            if (policyData) {
+              policyData.submissionId = policiesWithStatus[i].submissionId;
+              policyData.statusInfo = policiesWithStatus[i].statusInfo;
+              policyData.lastError = policiesWithStatus[i].lastError;
+              policyData.status = policiesWithStatus[i].status;
+              policyData.lastExecutionMode = policiesWithStatus[i].lastExecutionMode;
+              policyData.lastError = {
+                message: "Something gone wrong creating the output: Cassandra. Please re-check the policy.",
+                phase: "Input",
+                originalMsg: "FULL ERROR TRACE!!"
+              }
+            }else {
+              vm.policiesData.push(policiesWithStatus[i]);
+            }
           }
-          defer.resolve();
-        }, function () {
-          defer.reject();
-        });
+        }
+        defer.resolve();
+      }, function() {
+        defer.reject();
+      });
       return defer.promise;
     }
 
+    function isEllipsedStatusInfo(index) {
+      var text = angular.element.find('#policy-status-text-' + index)[0];
+      return text.offsetWidth < text.scrollWidth;
+    }
+
     function getPolicies() {
-      updatePoliciesStatus().then(function (result) {
+      var policiesStatus = PolicyFactory.getPoliciesStatus();
+      policiesStatus.then(function (result) {
+        vm.policiesData = result.policiesStatus;
         vm.loading = false;
-        checkPoliciesStatus = $interval(function () {
-          updatePoliciesStatus().then(null, function () {
+        checkPoliciesStatus = $interval(function() {
+          updatePoliciesStatus().then(null, function() {
             $interval.cancel(checkPoliciesStatus);
           });
         }, 5000);
-      }, function (error) {
+      }, function(error) {
         vm.loading = false;
         $interval.cancel(checkPoliciesStatus);
         vm.successMessage.text = '_ERROR_._' + error.data.i18nCode + '_';
@@ -197,19 +221,37 @@
     }
 
     function downloadPolicy(policyId) {
-      PolicyFactory.downloadPolicy(policyId).then(function (policyFile) {
+      PolicyFactory.downloadPolicy(policyId).then(function(policyFile) {
         var data = "text/json;charset=utf-8," + encodeURIComponent(JSON.stringify(policyFile));
         var a = document.createElement('a');
         a.href = 'data:' + data;
-        a.download =  policyFile.name + ".json";
+        a.download = policyFile.name + ".json";
         document.body.appendChild(a);
         a.click();
         a.remove();
       })
     }
+    
+    function showErrorModal(policyIndex) {
+      var policy = vm.policiesData[policyIndex];
+      var modalInstance = $uibModal.open({
+        animation: true,
+        templateUrl: 'templates/modal/policy-error-modal.tpl.html',
+        controller: 'PolicyErrorModalCtrl as vm',
+        size: 'lg',
+        resolve: {
+          policyName: function() {
+            return policy.name;
+          },
+          error: function() {
+            return policy.lastError;
+          }
+        }
+      });
+    }
 
     /*Stop $interval when changing the view*/
-    $scope.$on("$destroy", function () {
+    $scope.$on("$destroy", function() {
       if (checkPoliciesStatus) {
         $interval.cancel(checkPoliciesStatus);
       }
--- web/test/controllers/policies-controller.js
+++ web/test/controllers/policies-controller.js
@@ -1,20 +1,20 @@
-describe('policies.wizard.controller.policies-controller', function () {
+describe('policies.wizard.controller.policies-controller', function() {
   beforeEach(module('webApp'));
   beforeEach(module('api/policyList.json'));
   beforeEach(module('api/policiesStatusList.json'));
 
   var ctrl, scope, $interval, $timeout, fakePolicyList, fakePolicyStatusList, fakeError, policyModelFactoryMock, policyFactoryMock,
-    stateMock, fakeCreationStatus, modalServiceMock, resolvedPromise, rejectedPromise, fakeFinalPolicyJSON, wizardStatusServiceMock;
+      stateMock, fakeCreationStatus, modalServiceMock, resolvedPromise, rejectedPromise, fakeFinalPolicyJSON, wizardStatusServiceMock;
 
   // init mock modules
 
-  beforeEach(inject(function ($controller, $q, $httpBackend, $rootScope, _apiPolicyList_, _apiPoliciesStatusList_, _$interval_,_$timeout_) {
+  beforeEach(inject(function($controller, $q, $httpBackend, $rootScope, _apiPolicyList_, _apiPoliciesStatusList_, _$interval_, _$timeout_) {
     scope = $rootScope.$new();
     $interval = _$interval_;
     $timeout = _$timeout_;
     fakePolicyList = _apiPolicyList_;
     fakePolicyStatusList = _apiPoliciesStatusList_;
-    resolvedPromise = function () {
+    resolvedPromise = function() {
       var defer = $q.defer();
       defer.resolve();
 
@@ -22,7 +22,7 @@ describe('policies.wizard.controller.policies-controller', function () {
     };
 
     fakeError = {"data": {"i18nCode": "fake error message"}};
-    rejectedPromise = function () {
+    rejectedPromise = function() {
       var defer = $q.defer();
       defer.reject(fakeError);
 
@@ -30,16 +30,16 @@ describe('policies.wizard.controller.policies-controller', function () {
     };
 
     $httpBackend.when('GET', 'languages/en-US.json')
-      .respond({});
+        .respond({});
     fakeCreationStatus = {"currentStep": 0};
     policyFactoryMock = jasmine.createSpyObj('PolicyFactory', ['createPolicy', 'getAllPolicies', 'getPoliciesStatus', 'runPolicy', 'stopPolicy', 'downloadPolicy']);
-    policyFactoryMock.getAllPolicies.and.callFake(function () {
+    policyFactoryMock.getAllPolicies.and.callFake(function() {
       var defer = $q.defer();
       defer.resolve(fakePolicyList);
       return defer.promise;
     });
 
-    policyFactoryMock.getPoliciesStatus.and.callFake(function () {
+    policyFactoryMock.getPoliciesStatus.and.callFake(function() {
       var defer = $q.defer();
       defer.resolve(fakePolicyStatusList);
       return defer.promise;
@@ -55,7 +55,7 @@ describe('policies.wizard.controller.policies-controller', function () {
 
     modalServiceMock = jasmine.createSpyObj('ModalService', ['openModal']);
 
-    modalServiceMock.openModal.and.callFake(function () {
+    modalServiceMock.openModal.and.callFake(function() {
       var defer = $q.defer();
       defer.resolve();
       return {"result": defer.promise};
@@ -74,98 +74,70 @@ describe('policies.wizard.controller.policies-controller', function () {
 
   }));
 
-  describe("when it is initialized", function () {
-    describe("Should request the current policy list and their status", function () {
-      beforeEach(function () {
-        policyFactoryMock.getAllPolicies.calls.reset();
+  describe("when it is initialized", function() {
+    describe("Should request the current policy list and their status", function() {
+      beforeEach(function() {
         policyFactoryMock.getPoliciesStatus.calls.reset();
       });
 
-      it("if policy list is not empty, it should ask for their status", inject(function ($controller) {
-        ctrl = $controller('PolicyListCtrl', {
-          'WizardStatusService': wizardStatusServiceMock,
-          'PolicyModelFactory': policyModelFactoryMock,
-          'PolicyFactory': policyFactoryMock,
-          'ModalService': modalServiceMock,
-          '$state': stateMock,
-          '$scope': scope
-        });
-        scope.$digest();
 
-        expect(policyFactoryMock.getAllPolicies).toHaveBeenCalled();
-        expect(policyFactoryMock.getPoliciesStatus).toHaveBeenCalled();
-      }));
-
-      it("if policy list is empty, it must not ask for their status", inject(function ($controller) {
-        fakePolicyList = [];
-
-        ctrl = $controller('PolicyListCtrl', {
-          'WizardStatusService': wizardStatusServiceMock,
-          'PolicyModelFactory': policyModelFactoryMock,
-          'PolicyFactory': policyFactoryMock,
-          'ModalService': modalServiceMock,
-          '$state': stateMock,
-          '$scope': scope
-        });
-        scope.$digest();
-
-        expect(policyFactoryMock.getAllPolicies).toHaveBeenCalled();
-        expect(policyFactoryMock.getPoliciesStatus).not.toHaveBeenCalled();
-      }));
-
-      it("If server connection fails to ask for the policy list, the error is shown and policy status are not requested", inject(function ($controller) {
-        policyFactoryMock.getAllPolicies.and.callFake(rejectedPromise);
-        ctrl = $controller('PolicyListCtrl', {
-          'WizardStatusService': wizardStatusServiceMock,
-          'PolicyModelFactory': policyModelFactoryMock,
-          'PolicyFactory': policyFactoryMock,
-          'ModalService': modalServiceMock,
-          '$state': stateMock,
-          '$scope': scope
-        });
-
-        scope.$digest();
-
-        expect(ctrl.successMessage.text).toBe('_ERROR_._' + fakeError.data.i18nCode + '_');
-        expect(policyFactoryMock.getPoliciesStatus).not.toHaveBeenCalled();
-      }));
-
-      describe("each 5 seconds, it should update the status of policies", function () {
-        it("if server connection is lost, it should abort the updating of policy status", function () {
+      describe("each 5 seconds, it should update the status of policies", function() {
+        it("if server connection is lost, it should abort the updating of policy status", function() {
           policyFactoryMock.getPoliciesStatus.calls.reset();
           policyFactoryMock.getPoliciesStatus.and.callFake(rejectedPromise);
           $interval.flush(25000);
           expect(policyFactoryMock.getPoliciesStatus.calls.count()).toEqual(1);
         });
 
-        it("while server connection is right, it should update the policy status", function () {
-          var fakeNewStatus = 'new status';
-          fakePolicyStatusList.policiesStatus[0].status = fakeNewStatus;
+        describe("while server connection is right, it should update the policy status data", function() {
+          it('If policy was already painted, its status data is updated', function() {
+            var fakeNewStatus = {
+              status: 'stopped',
+              statusInfo: 'Status info data',
+              submissionId: 'fake submission id'
+            };
+            fakePolicyStatusList.policiesStatus[0].status = fakeNewStatus.status;
+            fakePolicyStatusList.policiesStatus[0].statusInfo = fakeNewStatus.statusInfo;
+            fakePolicyStatusList.policiesStatus[0].submissionId = fakeNewStatus.submissionId;
 
-          $interval.flush(5000);
-          expect(policyFactoryMock.getPoliciesStatus.calls.count()).toEqual(1);
-          $interval.flush(15000);
-          expect(policyFactoryMock.getPoliciesStatus.calls.count()).toEqual(4);
-          $interval.flush(5000);
-          scope.$digest();
+            $interval.flush(5000);
+            expect(policyFactoryMock.getPoliciesStatus.calls.count()).toEqual(1);
+            $interval.flush(15000);
+            expect(policyFactoryMock.getPoliciesStatus.calls.count()).toEqual(4);
+            $interval.flush(5000);
+            scope.$digest();
 
-          expect(ctrl.policiesData.list[0].status).toEqual(fakeNewStatus);
+            expect(ctrl.policiesData[0].status).toEqual(fakeNewStatus.status);
+            expect(ctrl.policiesData[0].statusInfo).toEqual(fakeNewStatus.statusInfo);
+            expect(ctrl.policiesData[0].submissionId).toEqual(fakeNewStatus.submissionId);
+          });
+
+          it ('if policy was not painted yet, it is added to policy list', function (){
+            var oldPolicyListLength = fakePolicyStatusList.policiesStatus.length;
+            var newPolicy = angular.copy(fakePolicyStatusList.policiesStatus[0]);
+            newPolicy.id = 'new policy id';
+
+            fakePolicyStatusList.policiesStatus.push(newPolicy);
+            $interval.flush(15000);
+            scope.$digest();
+            
+            expect(ctrl.policiesData.length).toEqual(oldPolicyListLength + 1);
+          });
         });
       });
     })
   });
 
-
-  describe("should be able to download a policy", function(){
-    it("policy factory is called to download the policy passing its id", function(){
+  describe("should be able to download a policy", function() {
+    it("policy factory is called to download the policy passing its id", function() {
       var fakePolicyId = "fake policy id";
       policyFactoryMock.downloadPolicy.and.callFake(resolvedPromise);
       ctrl.downloadPolicy(fakePolicyId);
 
       expect(policyFactoryMock.downloadPolicy).toHaveBeenCalledWith(fakePolicyId);
-      });
+    });
 
-    it ("a hidden element is created in order to force the file downloading", function(){
+    it("a hidden element is created in order to force the file downloading", function() {
 
     })
   })
