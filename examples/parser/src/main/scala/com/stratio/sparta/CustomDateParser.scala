71fc946afd07b03ebc7ae3ab19743433e159e96a
Jose Carlos Garcia Serrano <gserranojc@gmail.com>
2017-02-13 16:45:33 +0100

    Update CustomDateParser.scala

--- examples/parser/src/main/scala/com/stratio/sparta/CustomDateParser.scala
+++ examples/parser/src/main/scala/com/stratio/sparta/CustomDateParser.scala
@@ -13,7 +13,6 @@
  * See the License for the specific language governing permissions and
  * limitations under the License.
  */
-
 package com.stratio.sparta.plugin.transformation.json
 
 import com.stratio.sparta.sdk.pipeline.transformation.Parser
