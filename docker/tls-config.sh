22a542f05c6936e8a9e56d90ec82788599ad6999
Frannie-Ludmilla <francesca.pesci.fl@gmail.com>
2017-04-20 12:19:48 +0200

    [DCS-1368] Integration with Zookeeper securized with Kerberos (#1827)

--- docker/tls-config.sh
+++ docker/tls-config.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 _log_sparta_sec "Getting app cert from vault"
 #Downloads jks for the app
-#This wil be saved in /etc/sds/sparta/security/sparta.jks
+#This will be saved in /etc/sds/sparta/security/sparta.jks
 #The password for the jks is saved to $SPARTA_KEYSTORE_PASS
-getCert "userland" "$TENANT_NAME" "sparta" "JKS" "/etc/sds/sparta/security"
+getCert "userland" "$TENANT_NAME" "$TENANT_NAME" "JKS" "/etc/sds/sparta/security"
 
 _log_sparta_sec "Exporting sparta tls variables"
 ##Export variables for the app to configure tls
