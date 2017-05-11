2ed1e17df95ac555ef6551dcc4c6d789c38db918
Jose Carlos Garcia Serrano <gserranojc@gmail.com>
2017-05-11 10:28:56 +0100

    Added lost properties and bug with security protocol corrected (#1860)

--- docker/sparta-server-utils.sh
+++ docker/sparta-server-utils.sh
@@ -195,7 +195,7 @@ function configOptions() {
  sed -i "s|.*sparta.config.checkpointPath.*|sparta.config.checkpointPath = \""${SPARTA_CHECKPOINT_PATH}"\"|" ${SPARTA_CONF_FILE}
 
  if [[ ! -v SPARTA_AUTO_DELETE_CHECKPOINT ]]; then
-   SPARTA_AUTO_DELETE_CHECKPOINT=false
+   SPARTA_AUTO_DELETE_CHECKPOINT=true
  fi
  sed -i "s|.*sparta.config.autoDeleteCheckpoint.*|sparta.config.autoDeleteCheckpoint = ${SPARTA_AUTO_DELETE_CHECKPOINT}|" ${SPARTA_CONF_FILE}
 
