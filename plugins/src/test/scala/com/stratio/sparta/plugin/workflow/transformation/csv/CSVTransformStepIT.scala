2e1233d2879a012eab1559ea78fda1a661b31206
Jose Carlos Garcia Serrano <gserranojc@gmail.com>
2017-10-04 15:13:57 +0200

    TestIT added to Json and Csv transformation (#168)

---
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/transformation/csv/CSVTransformStepIT.scala
@@ -0,0 +1,95 @@
+/*
+ * Copyright (C) 2015 Stratio (http://stratio.com)
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *         http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.stratio.sparta.plugin.workflow.transformation.csv
+
+import java.io.{Serializable => JSerializable}
+
+import com.stratio.sparta.plugin.TemporalSparkContext
+import com.stratio.sparta.sdk.workflow.enumerators.SaveModeEnum
+import com.stratio.sparta.sdk.workflow.step.OutputOptions
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.catalyst.expressions.GenericRowWithSchema
+import org.apache.spark.sql.types.{DoubleType, StringType, StructField, StructType}
+import org.junit.runner.RunWith
+import org.scalatest.Matchers
+import org.scalatest.junit.JUnitRunner
+
+import scala.collection.mutable
+
+@RunWith(classOf[JUnitRunner])
+class CSVTransformStepIT extends TemporalSparkContext with Matchers {
+
+  "A CSVTransformStepIT" should "transform csv events the input DStream" in {
+
+    val fields =
+      """[
+        |{
+        |   "name":"color",
+        |   "type":"string"
+        |},
+        |{
+        |   "name":"price",
+        |   "type":"double"
+        |}]
+        |""".stripMargin
+    val inputField = "csv"
+    val inputSchema = StructType(Seq(StructField(inputField, StringType)))
+    val outputSchema = StructType(Seq(StructField("color", StringType), StructField("price", DoubleType)))
+    val dataQueue = new mutable.Queue[RDD[Row]]()
+    val dataIn = Seq(
+      new GenericRowWithSchema(Array("blue,12.1"), inputSchema),
+      new GenericRowWithSchema(Array("red,12.2"), inputSchema)
+    )
+    val dataOut = Seq(
+      new GenericRowWithSchema(Array("blue", 12.1), outputSchema),
+      new GenericRowWithSchema(Array("red", 12.2), outputSchema)
+    )
+    dataQueue += sc.parallelize(dataIn)
+    val stream = ssc.queueStream(dataQueue)
+    val inputData = Map("step1" -> stream)
+    val outputOptions = OutputOptions(SaveModeEnum.Append, "tableName", None, None)
+
+    val result = new CSVTransformStep(
+      "dummy",
+      outputOptions,
+      ssc,
+      sparkSession,
+      Map("fields" -> fields.asInstanceOf[JSerializable],
+        "inputField" -> inputField,
+        "addAllInputFields" -> false)
+    ).transform(inputData)
+    val totalEvents = ssc.sparkContext.accumulator(0L, "Number of events received")
+
+    result.foreachRDD(rdd => {
+      val streamingEvents = rdd.count()
+      log.info(s" EVENTS COUNT : \t $streamingEvents")
+      totalEvents += streamingEvents
+      log.info(s" TOTAL EVENTS : \t $totalEvents")
+      val streamingRegisters = rdd.collect()
+      if (!rdd.isEmpty())
+        streamingRegisters.foreach { row =>
+          assert(dataOut.contains(row))
+          assert(outputSchema == row.schema)
+        }
+    })
+    ssc.start()
+    ssc.awaitTerminationOrTimeout(10000L)
+
+    assert(totalEvents.value === 2)
+  }
+}
