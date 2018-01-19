ee8e4ab6eed4da6f59d8b6c4e5680592d2323aed
Max <ma.tello.ortiz@gmail.com>
2018-01-19 10:37:08 +0100

    [SPARTA-1374]Repartition transformation with batch support (#327)

--- plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/repartition/RepartitionTransformStep.scala
+++ plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/repartition/RepartitionTransformStep.scala
@@ -19,28 +19,25 @@ package com.stratio.sparta.plugin.workflow.transformation.repartition
 import java.io.{Serializable => JSerializable}
 
 import akka.event.slf4j.SLF4JLogging
-import com.stratio.sparta.sdk.DistributedMonad
 import com.stratio.sparta.sdk.properties.ValidatingPropertyMap._
+import com.stratio.sparta.sdk.DistributedMonad
 import com.stratio.sparta.sdk.workflow.step.{OutputOptions, TransformStep}
-import org.apache.spark.sql.Row
 import org.apache.spark.sql.crossdata.XDSession
 import org.apache.spark.streaming.StreamingContext
-import org.apache.spark.streaming.dstream.DStream
+import org.apache.spark.sql.Row
 
 import scala.util.Try
 
-class RepartitionTransformStep(name: String,
-                               outputOptions: OutputOptions,
-                               ssc: Option[StreamingContext],
-                               xDSession: XDSession,
-                               properties: Map[String, JSerializable])
-  extends TransformStep[DStream](name, outputOptions, ssc, xDSession, properties) with SLF4JLogging {
+abstract class RepartitionTransformStep[Underlying[Row]](
+                                                         name: String,
+                                                         outputOptions: OutputOptions,
+                                                         ssc: Option[StreamingContext],
+                                                         xDSession: XDSession,
+                                                         properties: Map[String, JSerializable]
+                                                        )(implicit dsMonadEvidence: Underlying[Row] => DistributedMonad[Underlying])
+  extends TransformStep[Underlying](name, outputOptions, ssc, xDSession, properties) with SLF4JLogging {
 
-  lazy val partitions: Int = Try(properties.getInt("partitions")).getOrElse(
+  lazy val partitions = Try(properties.getInt("partitions")).getOrElse(
     throw new Exception("Property partitions is mandatory"))
 
-  override def transform(inputData: Map[String, DistributedMonad[DStream]]): DistributedMonad[DStream] =
-    applyHeadTransform(inputData) { (inputSchema, inputStream) =>
-      inputStream.ds.repartition(partitions)
-    }
 }
