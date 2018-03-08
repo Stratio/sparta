b38b78fcd65baf730430f2219a39aa828ed091d8
Max <ma.tello.ortiz@gmail.com>
2018-03-08 18:43:46 +0100

    [SPARTA-1546] Update license on master (#476)

--- plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/distinct/DistinctTransformStepStreaming.scala
+++ plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/distinct/DistinctTransformStepStreaming.scala
@@ -1,19 +1,8 @@
 /*
- * Copyright (C) 2015 Stratio (http://stratio.com)
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
  *
- * Licensed under the Apache License, Version 2.0 (the "License");
- * you may not use this file except in compliance with the License.
- * You may obtain a copy of the License at
- *
- *         http://www.apache.org/licenses/LICENSE-2.0
- *
- * Unless required by applicable law or agreed to in writing, software
- * distributed under the License is distributed on an "AS IS" BASIS,
- * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
- * See the License for the specific language governing permissions and
- * limitations under the License.
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
  */
-
 package com.stratio.sparta.plugin.workflow.transformation.distinct
 
 import java.io.{Serializable => JSerializable}
--- web/src/app/modules/wizard/components/selected-entity/selected-entity.component.ts
+++ web/src/app/modules/wizard/components/selected-entity/selected-entity.component.ts
@@ -1,3 +1,8 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
+ *
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
+ */
 import { AnyFn } from '@ngrx/store/src/selector';
 ///
 /// Copyright (C) 2015 Stratio (http://stratio.com)
