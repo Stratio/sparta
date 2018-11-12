6ede6e102fff042dece96d8febcd4f9a78241379
mleida-stratio <mleida@stratio.com>
2018-11-12 14:33:32 +0100

    Implement FPGrowth Step Unit Tests

--- plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/algorithms/FPGrowthUT.scala
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/algorithms/FPGrowthUT.scala
@@ -7,73 +7,42 @@ package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.al
 
 import java.io.{Serializable => JSerializable}
 
+import com.github.fommil.netlib.BLAS.{getInstance => blas}
 import com.stratio.sparta.core.enumerators.SaveModeEnum
 import com.stratio.sparta.core.models.ErrorValidations
 import com.stratio.sparta.core.properties.JsoneyString
 import com.stratio.sparta.plugin.TemporalSparkContext
 import com.stratio.sparta.plugin.enumerations.MlPipelineSaveMode
 import com.stratio.sparta.plugin.workflow.output.mlpipeline.MlPipelineOutputStep
+import org.apache.spark.ml.recommendation.ALS.Rating
 import org.apache.spark.sql.DataFrame
-
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, _}
-import com.github.fommil.netlib.BLAS.{getInstance => blas}
-import org.apache.spark.ml.recommendation.ALS.Rating
 
+import scala.collection.mutable
+import scala.collection.mutable.ArrayBuffer
 import scala.io.Source
 import scala.util.{Failure, Random, Try}
 
 @RunWith(classOf[JUnitRunner])
-class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndAfterAll {
+class FPGrowthUT extends TemporalSparkContext with ShouldMatchers with BeforeAndAfterAll {
 
   self: FlatSpec =>
 
   trait ReadDescriptorResource{
     def getJsonDescriptor(filename:String): String = {
-      Source.fromInputStream(getClass.getResourceAsStream("/mlpipeline/singlesteps/algorithms/als/" + filename)).mkString
+      Source.fromInputStream(getClass.getResourceAsStream("/mlpipeline/singlesteps/algorithms/fpgrowth/" + filename)).mkString
     }
   }
 
   trait WithExampleData {
-
-    private def genFactors(
-                            size: Int,
-                            rank: Int,
-                            random: Random,
-                            a: Float = -1.0f,
-                            b: Float = 1.0f): Seq[(Int, Array[Float])] = {
-      require(size > 0 && size < Int.MaxValue / 3)
-      require(b > a)
-      val ids = mutable.Set.empty[Int]
-      while (ids.size < size) {
-        ids += random.nextInt()
-      }
-      val width = b - a
-      ids.toSeq.sorted.map(id => (id, Array.fill(rank)(a + random.nextFloat() * width)))
-    }
-
-    // The assumption of the implicit feedback model is that unobserved ratings are more likely to
-    // be negatives.
-    val positiveFraction = 0.8
-    val negativeFraction = 1.0 - positiveFraction
-    val random = new Random(1234)
-    val userFactors = genFactors(20, 2, random)
-    val itemFactors = genFactors(40, 2, random)
-    val data = ArrayBuffer.empty[Rating[Int]]
-    for ((userId, userFactor) <- userFactors; (itemId, itemFactor) <- itemFactors) {
-      val rating = blas.sdot(2, userFactor, 1, itemFactor, 1)
-      val threshold = if (rating > 0) positiveFraction else negativeFraction
-      val observed = random.nextDouble() < threshold
-      if (observed) {
-        val x = random.nextDouble()
-        val noise = 0.01 * random.nextGaussian()
-        data += Rating(userId, itemId, rating + noise.toFloat)
-      }
-    }
-      val training: DataFrame = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(data))
+      val training: DataFrame = sparkSession.createDataFrame(Seq(
+        (0, Array("1", "2")),
+        (0, Array("1", "2")),
+        (0, Array("1", "2")),
+        (0, Array("1", "3"))
+      )).toDF("id", "items")
   }
 
   trait WithFilesystemProperties {
@@ -122,7 +91,7 @@ class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndA
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-default-values-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("fpgrowth-default-params-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
@@ -142,13 +111,15 @@ class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndA
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-empty-params-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("fpgrowht-emtpy-params-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
       assert(validation.isSuccess)
 
-      executeStepAndUsePipeline(training, properties)
+      val execution = Try{executeStepAndUsePipeline(training, properties)}
+      assert(execution.isSuccess)
+
     }
 
   /* -------------------------------------------------------------
@@ -159,7 +130,7 @@ class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndA
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-wrong-columnname-values-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("fpgrowth-wrong-columnname-values-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
