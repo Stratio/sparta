238f14319939a15104d3eb64032bc9d4b6808dd7
mleida-stratio <mleida@stratio.com>
2018-11-12 12:28:36 +0100

    added UT for LinearSVC

--- plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/algorithms/LinearSVCStepUT.scala
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/algorithms/LinearSVCStepUT.scala
@@ -7,75 +7,74 @@ package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.al
 
 import java.io.{Serializable => JSerializable}
 
+import breeze.optimize.linear.PowerMethod.BDV
 import com.stratio.sparta.core.enumerators.SaveModeEnum
 import com.stratio.sparta.core.models.ErrorValidations
 import com.stratio.sparta.core.properties.JsoneyString
 import com.stratio.sparta.plugin.TemporalSparkContext
 import com.stratio.sparta.plugin.enumerations.MlPipelineSaveMode
 import com.stratio.sparta.plugin.workflow.output.mlpipeline.MlPipelineOutputStep
-import org.apache.spark.sql.DataFrame
-
-import scala.collection.mutable
-import scala.collection.mutable.ArrayBuffer
-import scala.collection.mutable.WrappedArray
+import org.apache.spark.ml.feature.LabeledPoint
+import org.apache.spark.ml.linalg.{DenseVector, SparseVector, Vector, Vectors}
+import org.apache.spark.sql.{DataFrame, Dataset, Row}
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 import org.scalatest.{BeforeAndAfterAll, _}
-import com.github.fommil.netlib.BLAS.{getInstance => blas}
-import org.apache.spark.ml.recommendation.ALS.Rating
-import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.functions.udf
 
 import scala.io.Source
 import scala.util.{Failure, Random, Try}
 
 @RunWith(classOf[JUnitRunner])
-class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndAfterAll {
+class LinearSVCStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndAfterAll {
 
   self: FlatSpec =>
 
+  @transient var smallBinaryDataset: Dataset[_] = _
+  @transient var smallValidationDataset: Dataset[_] = _
+  @transient var binaryDataset: Dataset[_] = _
+
+  @transient var smallSparseBinaryDataset: Dataset[_] = _
+  @transient var smallSparseValidationDataset: Dataset[_] = _
+
+
   trait ReadDescriptorResource{
     def getJsonDescriptor(filename:String): String = {
-      Source.fromInputStream(getClass.getResourceAsStream("/mlpipeline/singlesteps/algorithms/als/" + filename)).mkString
+      Source.fromInputStream(getClass.getResourceAsStream("/mlpipeline/singlesteps/algorithms/linearsvc/" + filename)).mkString
     }
   }
 
   trait WithExampleData {
-
-    private def genFactors(
-                            size: Int,
-                            rank: Int,
-                            random: Random,
-                            a: Float = -1.0f,
-                            b: Float = 1.0f): Seq[(Int, Array[Float])] = {
-      require(size > 0 && size < Int.MaxValue / 3)
-      require(b > a)
-      val ids = mutable.Set.empty[Int]
-      while (ids.size < size) {
-        ids += random.nextInt()
+    // Generate noisy input of the form Y = signum(x.dot(weights) + intercept + noise)
+    def generateSVMInput(
+                          intercept: Double,
+                          weights: Array[Double],
+                          nPoints: Int,
+                          seed: Int): Seq[LabeledPoint] = {
+      val rnd = new Random(seed)
+      val weightsMat = new BDV(weights)
+      val x = Array.fill[Array[Double]](nPoints)(
+        Array.fill[Double](weights.length)(rnd.nextDouble() * 2.0 - 1.0))
+      val y = x.map { xi =>
+        val yD = new BDV(xi).dot(weightsMat) + intercept + 0.01 * rnd.nextGaussian()
+        if (yD > 0) 1.0 else 0.0
       }
-      val width = b - a
-      ids.toSeq.sorted.map(id => (id, Array.fill(rank)(a + random.nextFloat() * width)))
+      y.zip(x).map(p => LabeledPoint(p._1, Vectors.dense(p._2)))
     }
 
-    // The assumption of the implicit feedback model is that unobserved ratings are more likely to
-    // be negatives.
-    val positiveFraction = 0.8
-    val negativeFraction = 1.0 - positiveFraction
-    val random = new Random(1234)
-    val userFactors = genFactors(20, 2, random)
-    val itemFactors = genFactors(40, 2, random)
-    val data = ArrayBuffer.empty[Rating[Int]]
-    for ((userId, userFactor) <- userFactors; (itemId, itemFactor) <- itemFactors) {
-      val rating = blas.sdot(2, userFactor, 1, itemFactor, 1)
-      val threshold = if (rating > 0) positiveFraction else negativeFraction
-      val observed = random.nextDouble() < threshold
-      if (observed) {
-        val x = random.nextDouble()
-        val noise = 0.01 * random.nextGaussian()
-        data += Rating(userId, itemId, rating + noise.toFloat)
-      }
-    }
-      val training: DataFrame = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(data))
+    // NOTE: Intercept should be small for generating equal 0s and 1s
+    val A = 0.01
+    val B = -1.5
+    val C = 1.0
+    smallBinaryDataset = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(generateSVMInput(A, Array[Double](B, C), 50, 42)))
+    smallValidationDataset = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(generateSVMInput(A, Array[Double](B, C), 50, 17)))
+    binaryDataset = sparkSession.createDataFrame(sparkSession.sparkContext.parallelize(generateSVMInput(1.0, Array[Double](1.0, 2.0, 3.0, 4.0), 10000, 42)))
+
+    // Dataset for testing SparseVector
+    val toSparse: Vector => SparseVector = _.asInstanceOf[DenseVector].toSparse
+    val sparse = udf(toSparse)
+    //smallSparseBinaryDataset = smallBinaryDataset.withColumn("features", sparse('features))
+    //smallSparseValidationDataset = smallValidationDataset.withColumn("features", sparse('features))
   }
 
   trait WithFilesystemProperties {
@@ -120,52 +119,52 @@ class ALSStepUT extends TemporalSparkContext with ShouldMatchers with BeforeAndA
    => Correct Pipeline construction and execution
     ------------------------------------------------------------- */
 
-  "ALS with default configuration values" should "provide a valid SparkMl pipeline than it can be trained in a workflow (given valid user and item columns)" in
+  "LinearSVC default configuration values" should "provide a valid SparkMl pipeline than it can be trained in a workflow (given valid user and item columns)" in
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-default-values-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("linearsvc-default-values-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
       assert(validation.isSuccess)
       assert(validation.get.valid)
 
-      executeStepAndUsePipeline(training, properties)
+      executeStepAndUsePipeline(smallValidationDataset.toDF, properties)
     }
 
   /* -------------------------------------------------------------
 => Wrong Pipeline construction and execution
 ------------------------------------------------------------- */
 
-  "ALS with empty configuration values" should "provide a valid SparkMl pipeline than it can be trained in a workflow (given default user and item columns)" in
+  "LinearSVC with empty configuration values" should "provide a valid SparkMl pipeline than it can be trained in a workflow (given default user and item columns)" in
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-empty-params-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("linearsvc-empty-params-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
       assert(validation.isSuccess)
 
-      executeStepAndUsePipeline(training, properties)
+      executeStepAndUsePipeline(smallValidationDataset.toDF, properties)
     }
 
   /* -------------------------------------------------------------
  => Wrong Pipeline construction and execution
   ------------------------------------------------------------- */
 
-  "ALS with empty configuration values" should "not provide a valid SparkMl pipeline than it can be trained in a workflow (given no valid user and item columns)" in
+  "LinearSVC with empty configuration values" should "not provide a valid SparkMl pipeline than it can be trained in a workflow (given no valid user and item columns)" in
     new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
       with WithFilesystemProperties{
 
-      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("als-wrong-columnname-values-v0.json")))
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("linearsvc-wrong-columnname-values-v0.json")))
 
       // Validation step mut be done correctly
       val validation = Try{validateMlPipelineStep(properties)}
       assert(validation.isSuccess)
       //TODO add expected error tye assert(validation.get.valid)
-      val execution = Try{executeStepAndUsePipeline(training, properties)}
+      val execution = Try{executeStepAndUsePipeline(smallValidationDataset.toDF, properties)}
       assert(execution.isFailure)
       execution match { case Failure(t) => log.info(t.toString) }
     }
