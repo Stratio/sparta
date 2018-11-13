55982be66ea03474d0e9c917c6ac41ff7d250ad2
Guillermo Ríos <grios@stratio.com>
2018-11-13 15:45:35 +0100

    Move prepsocessing step tests to preprocessing folder

--- plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/Word2VecStepUT.scala
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/Word2VecStepUT.scala
@@ -3,31 +3,27 @@
  *
  * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
  */
-package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.algorithms
+package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.preprocessing
 
-import org.apache.spark.ml.linalg.Vectors
+import com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.GenericPipelineStepTest
 import org.apache.spark.sql.DataFrame
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
 @RunWith(classOf[JUnitRunner])
-class IdfStepUT extends GenericPipelineStepTest {
+class Word2VecStepUT extends GenericPipelineStepTest {
 
-  override def stepName: String = "idf"
+  override def stepName: String = "word2vec"
 
-  override def resourcesPath: String = "/mlpipeline/singlesteps/preprocessing/idf/"
+  override def resourcesPath: String = "/mlpipeline/singlesteps/preprocessing/word2vec/"
 
   override def trainingDf: DataFrame = {
-    val sparkValToImportImplicits = sparkSession
-    import sparkValToImportImplicits.implicits._
-
-    val numOfFeatures = 4
-    Seq(
-      (1, Vectors.sparse(numOfFeatures, Array(1, 3), Array(1.0, 2.0))),
-      (2, Vectors.dense(0.0, 1.0, 2.0, 3.0)),
-      (3, Vectors.sparse(numOfFeatures, Array(1), Array(1.0)))
-    ).toDF("id", "features")
-
+    val sentence = "a b " * 100 + "a c " * 10
+    sparkSession.createDataFrame(
+      Seq(sentence, sentence)
+        .map(_.split(" "))
+        .map(Tuple1.apply)
+    ).toDF("text")
   }
 
 }
