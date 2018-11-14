eaa8a11ee87d0005307836965f15aec34f0e6161
Alvaro Gala <agala@stratio.com>
2018-11-14 11:09:50 +0100

    [INTELL-1972] DCT and OneHotEncoder Unit Test added

--- plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/OneHotEncoderStepUT.scala
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/OneHotEncoderStepUT.scala
@@ -3,31 +3,26 @@
  *
  * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
  */
-package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.algorithms
+package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.preprocessing
 
 import com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.GenericPipelineStepTest
-import org.apache.spark.ml.feature.LabeledPoint
-import org.apache.spark.ml.linalg.Vectors
 import org.apache.spark.sql.DataFrame
 import org.junit.runner.RunWith
 import org.scalatest.junit.JUnitRunner
 
-
 @RunWith(classOf[JUnitRunner])
-class LinearRegression extends GenericPipelineStepTest {
+class OneHotEncoderStepUT extends GenericPipelineStepTest {
 
-  override def stepName: String = "linearregression"
+  override def stepName: String = "onehotencoder"
 
-  override def resourcesPath: String = "/mlpipeline/singlesteps/algorithms/linearregression/"
+  override def resourcesPath: String = "/mlpipeline/singlesteps/preprocessing/onehotencoder/"
 
-  override def trainingDf: DataFrame = {
-
-    val data = (0.0 to 9.0 by 1) // create a collection of Doubles
-      .map(n => (n, n)) // make it pairs
-      .map { case (label, features) =>
-      LabeledPoint(label, Vectors.dense(features)) // create labeled points of dense vectors
-    }
-
-    sparkSession.createDataFrame(data).toDF()
-  }
+  override def trainingDf: DataFrame = sparkSession.createDataFrame(Seq(
+    (0, "a", 0.0),
+    (1, "b", 2.0),
+    (2, "c", 1.0),
+    (3, "a", 0.0),
+    (4, "a", 0.0),
+    (5, "c", 1.0)
+  )).toDF("id", "category", "string_indexed")
 }
