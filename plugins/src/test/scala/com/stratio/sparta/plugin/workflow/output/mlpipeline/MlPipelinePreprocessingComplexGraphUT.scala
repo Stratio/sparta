c649a0d8ceaa826c8a807bfb04c32fca7ee84c43
mleida-stratio <mleida@stratio.com>
2018-11-15 15:25:00 +0100

    fix missing header in MLPipelineFilteredStages and created complex pipeline testing class

---
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/MlPipelinePreprocessingComplexGraphUT.scala
@@ -0,0 +1,110 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
+ *
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
+ */
+package com.stratio.sparta.plugin.workflow.output.mlpipeline
+
+import java.io.{Serializable => JSerializable}
+
+import com.stratio.sparta.core.enumerators.SaveModeEnum
+import com.stratio.sparta.core.models.ErrorValidations
+import com.stratio.sparta.core.properties.JsoneyString
+import com.stratio.sparta.plugin.TemporalSparkContext
+import com.stratio.sparta.plugin.enumerations.MlPipelineSaveMode
+import org.apache.spark.sql.DataFrame
+import org.apache.spark.sql.types._
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+import org.scalatest.{BeforeAndAfterAll, _}
+
+import scala.io.Source
+import scala.util.Try
+
+@RunWith(classOf[JUnitRunner])
+class MlPipelinePreprocessingComplexGraphUT extends TemporalSparkContext with ShouldMatchers with BeforeAndAfterAll {
+
+  self: FlatSpec =>
+
+  trait ReadDescriptorResource{
+    def getJsonDescriptor(filename:String): String = {
+      Source.fromInputStream(getClass.getResourceAsStream("/mlpipeline/" + filename)).mkString
+    }
+  }
+
+  trait WithExampleData {
+    val schema = StructType(
+      List(StructField("country",StringType,true),
+        StructField("description",StringType,true),
+        StructField("designation",StringType,true),
+        StructField("points",LongType,true),
+        StructField("price",DoubleType,true),
+        StructField("province",StringType,true),
+        StructField("region_1",StringType,true),
+        StructField("region_2",StringType,true),
+        StructField("taster_name",StringType,true),
+        StructField("taster_twitter_handle",StringType,true),
+        StructField("title",StringType,true),
+        StructField("variety",StringType,true),
+        StructField("winery",StringType,true)))
+    val training: DataFrame = sparkSession.read.schema(schema).format("csv").
+      option("header","true").load(getClass.getResource("/mlpipeline/wine.csv").getPath)
+  }
+
+  trait WithFilesystemProperties {
+    var properties:Map[String, JSerializable] = Map(
+      "output.mode" -> JsoneyString(MlPipelineSaveMode.FILESYSTEM.toString),
+      "path" -> JsoneyString("/tmp/pipeline_tests")
+    )
+  }
+
+  trait WithExecuteStep{
+    def executeStep(training:DataFrame, properties:Map[String, JSerializable]){
+      // · Creating outputStep
+      val mlPipelineOutput = new MlPipelineOutputStep("MlPipeline.out", sparkSession, properties)
+      // · Executing step
+      mlPipelineOutput.save(training, SaveModeEnum.Overwrite, Map.empty[String, String])
+    }
+
+    def executeStepAndUsePipeline(training:DataFrame, properties:Map[String, JSerializable]){
+      // · Creating outputStep
+      val mlPipelineOutput = new MlPipelineOutputStep("MlPipeline.out", sparkSession, properties)
+      // · Executing step
+      mlPipelineOutput.save(training, SaveModeEnum.Overwrite, Map.empty[String, String])
+      // · Use pipeline object
+      val pipelineModel = mlPipelineOutput.pipeline.get.fit(training)
+      val df = pipelineModel.transform(training)
+      df.show()
+    }
+  }
+
+  trait WithValidateStep{
+    def validateMlPipelineStep(properties:Map[String, JSerializable]): ErrorValidations = {
+      // · Creating outputStep
+      val mlPipelineOutput = new MlPipelineOutputStep("MlPipeline.out", sparkSession, properties)
+      // · Executing step
+      val e = mlPipelineOutput.validate()
+      e.messages.foreach(x => log.info(x.message))
+      e
+    }
+  }
+
+  /* -------------------------------------------------------------
+   => Correct Pipeline construction and execution
+    ------------------------------------------------------------- */
+
+  "MlPipeline" should "construct a valid SparkMl pipeline than it can be trained in a workflow" in
+    new ReadDescriptorResource with WithExampleData with WithExecuteStep with WithValidateStep
+      with WithFilesystemProperties{
+
+      properties = properties.updated("pipeline", JsoneyString(getJsonDescriptor("complexTest.json")))
+
+      // Validation step mut be done correctly
+      val validation = Try{validateMlPipelineStep(properties)}
+      assert(validation.isSuccess)
+      assert(validation.get.valid)
+
+      executeStepAndUsePipeline(training, properties)
+    }
+
+}
