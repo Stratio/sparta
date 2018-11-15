d5235ac9cb3b01fb8116419f0a006f1a1852a16a
Guillermo Ríos <grios@stratio.com>
2018-11-15 10:15:19 +0100

    Implement StandardScaler Pipeline Step Unit Test

---
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/StandardScalerStepUT.scala
@@ -0,0 +1,27 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
+ *
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
+ */
+package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.preprocessing
+
+import com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.GenericPipelineStepTest
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.DataFrame
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class StandardScalerStepUT extends GenericPipelineStepTest {
+
+  override def stepName: String = "standardscaler"
+
+  override def resourcesPath: String = "/mlpipeline/singlesteps/preprocessing/standardscaler/"
+
+  override def trainingDf: DataFrame = sparkSession.createDataFrame(Seq(
+    (0, Vectors.dense(1.0, 0.5, -1.0)),
+    (1, Vectors.dense(2.0, 1.0, 1.0)),
+    (2, Vectors.dense(4.0, 10.0, 2.0))
+  )).toDF("id", "features")
+
+}
