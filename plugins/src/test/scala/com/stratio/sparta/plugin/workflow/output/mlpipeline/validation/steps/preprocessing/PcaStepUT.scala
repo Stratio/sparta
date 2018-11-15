1791de5ce83ced80f5df1e76ad8c5a5de342f06f
Guillermo Ríos <grios@stratio.com>
2018-11-15 08:51:54 +0100

    Implement PCA Pipeline Step Unit Tests

---
+++ plugins/src/test/scala/com/stratio/sparta/plugin/workflow/output/mlpipeline/validation/steps/preprocessing/PcaStepUT.scala
@@ -0,0 +1,33 @@
+/*
+ * © 2017 Stratio Big Data Inc., Sucursal en España. All rights reserved.
+ *
+ * This software – including all its source code – contains proprietary information of Stratio Big Data Inc., Sucursal en España and may not be revealed, sold, transferred, modified, distributed or otherwise made available, licensed or sublicensed to third parties; nor reverse engineered, disassembled or decompiled, without express written authorization from Stratio Big Data Inc., Sucursal en España.
+ */
+package com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.steps.preprocessing
+
+import com.stratio.sparta.plugin.workflow.output.mlpipeline.validation.GenericPipelineStepTest
+import org.apache.spark.ml.linalg.Vectors
+import org.apache.spark.sql.DataFrame
+import org.junit.runner.RunWith
+import org.scalatest.junit.JUnitRunner
+
+@RunWith(classOf[JUnitRunner])
+class PcaStepUT extends GenericPipelineStepTest {
+
+  override def stepName: String = "pca"
+
+  override def resourcesPath: String = "/mlpipeline/singlesteps/preprocessing/pca/"
+
+  override def trainingDf: DataFrame = {
+    val dimension = 5
+    val data = Array(
+      Vectors.sparse(dimension, Seq((1, 1.0), (3, 7.0))),
+      Vectors.dense(2.0, 0.0, 3.0, 4.0, 5.0),
+      Vectors.dense(4.0, 0.0, 0.0, 6.0, 7.0)
+    )
+    sparkSession.createDataFrame(data.map(Tuple1.apply)).toDF("features")
+  }
+
+  override def emptyParamsAvailable: Boolean = false
+  
+}
