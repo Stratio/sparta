56abe0831363447555180cd25f0013351c99abbe
César Gil Moreno <45395584+cgil-stratio@users.noreply.github.com>
2019-03-11 20:08:28 +0100

    [SPARTA-2141] Add columns accept sql (#1289)
    * Front ready
    
    * Validations work in progress
    
    * Fixed front and workflow working
    
    * Added test that checks columns added by expression
    
    * Fixed Compae comments in PR

--- plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/column/AddColumnsTransformStep.scala
+++ plugins/src/main/scala/com/stratio/sparta/plugin/workflow/transformation/column/AddColumnsTransformStep.scala
@@ -8,36 +8,55 @@ package com.stratio.sparta.plugin.workflow.transformation.column
 
 import java.io.{Serializable => JSerializable}
 
-import scala.util.{Failure, Success, Try}
 import akka.event.slf4j.SLF4JLogging
-import org.apache.spark.rdd.RDD
-import org.apache.spark.sql.Row
-import org.apache.spark.sql.crossdata.XDSession
-import org.apache.spark.sql.types.{StructField, StructType}
-import org.apache.spark.streaming.StreamingContext
-import org.json4s.jackson.Serialization.read
-import org.json4s.{DefaultFormats, Formats}
-import com.stratio.sparta.plugin.helper.SchemaHelper.{createOrReplaceTemporalViewDf, getSchemaFromSessionOrModelOrRdd, parserInputSchema}
-import com.stratio.sparta.plugin.helper.SparkStepHelper
-import com.stratio.sparta.plugin.models.PropertyQuery
 import com.stratio.sparta.core.DistributedMonad
 import com.stratio.sparta.core.helpers.SdkSchemaHelper
 import com.stratio.sparta.core.models._
 import com.stratio.sparta.core.properties.JsoneyStringSerializer
 import com.stratio.sparta.core.properties.ValidatingPropertyMap._
 import com.stratio.sparta.core.workflow.step.TransformStep
+import com.stratio.sparta.plugin.enumerations.SelectType
+import com.stratio.sparta.plugin.enumerations.SelectType.SelectType
+import com.stratio.sparta.plugin.helper.SchemaHelper.{createOrReplaceTemporalViewDf, getSchemaFromSessionOrModelOrRdd, parserInputSchema}
+import com.stratio.sparta.plugin.helper.SparkStepHelper
+import com.stratio.sparta.plugin.models.{PropertyQuery, SelectExpressionModel}
+import com.stratio.sparta.serving.core.helpers.ErrorValidationsHelper
+import com.stratio.sparta.serving.core.helpers.ErrorValidationsHelper.HasError
+import org.apache.spark.rdd.RDD
+import org.apache.spark.sql.Row
+import org.apache.spark.sql.crossdata.XDSession
+import org.apache.spark.sql.functions._
+import org.apache.spark.sql.types.{StructField, StructType}
+import org.apache.spark.streaming.StreamingContext
+import org.json4s.jackson.Serialization.read
+import org.json4s.{DefaultFormats, Formats}
+
+import scala.util.{Failure, Success, Try}
 
 //scalastyle:off
 abstract class AddColumnsTransformStep[Underlying[Row]](
-                                                                  name: String,
-                                                                  outputOptions: OutputOptions,
-                                                                  transformationStepsManagement: TransformationStepManagement,
-                                                                  ssc: Option[StreamingContext],
-                                                                  xDSession: XDSession,
-                                                                  properties: Map[String, JSerializable]
-                                                                )(implicit dsMonadEvidence: Underlying[Row] => DistributedMonad[Underlying])
+                                                         name: String,
+                                                         outputOptions: OutputOptions,
+                                                         transformationStepsManagement: TransformationStepManagement,
+                                                         ssc: Option[StreamingContext],
+                                                         xDSession: XDSession,
+                                                         properties: Map[String, JSerializable]
+                                                       )(implicit dsMonadEvidence: Underlying[Row] => DistributedMonad[Underlying])
   extends TransformStep[Underlying](name, outputOptions, transformationStepsManagement, ssc, xDSession, properties)
     with SLF4JLogging {
+
+  lazy val selectType: SelectType = Try {
+    SelectType.withName(properties.getString("selectType", "COLUMNS").toUpperCase())
+  }.getOrElse(SelectType.EXPRESSION)
+
+
+  lazy val expressionList: Seq[SelectExpressionModel] = {
+    implicit val json4sJacksonFormats: Formats = DefaultFormats + new JsoneyStringSerializer()
+    val expression = s"${properties.getString("addColumnExpressionList", None).notBlank.fold("[]") { values => values.toString }}"
+
+    read[Seq[SelectExpressionModel]](expression)
+  }
+
 
   lazy val columns: Seq[PropertyQuery] = {
     implicit val json4sJacksonFormats: Formats = DefaultFormats + new JsoneyStringSerializer()
@@ -45,6 +64,7 @@ abstract class AddColumnsTransformStep[Underlying[Row]](
 
     read[Seq[PropertyQuery]](cols)
   }
+
 
   lazy val newColumnsSchemaTypes: Map[String, Any] = columns.map { outputField =>
     val outputType = outputField.`type`.notBlank.getOrElse("string")
@@ -59,53 +79,39 @@ abstract class AddColumnsTransformStep[Underlying[Row]](
     outputField.field -> castingToOutputSchema(schema, outputField.query)
   }.toMap
 
-  override def validate(options: Map[String, String] = Map.empty[String, String]): ErrorValidations = {
-    var validation = ErrorValidations(valid = true, messages = Seq.empty)
 
-    if (!SdkSchemaHelper.isCorrectTableName(name))
-      validation = ErrorValidations(
-        valid = false,
-        messages = validation.messages :+ WorkflowValidationMessage(s"The step name $name is not valid.", name))
+  def validateExpression: Seq[(Boolean, String)] = {
+
+    expressionList.collect {
+      case selectExpression if Try(expr(selectExpression.addColumnExpression)).isFailure =>
+        true -> s"$selectExpression expression is not valid."
+    }
+  }
+
+
+  override def validate(options: Map[String, String] = Map.empty[String, String]): ErrorValidations = {
 
     //If contains schemas, validate if it can be parsed
-    if (inputsModel.inputSchemas.nonEmpty) {
-      inputsModel.inputSchemas.foreach { input =>
-        if (parserInputSchema(input.schema).isFailure)
-          validation = ErrorValidations(
-            valid = false,
-            messages = validation.messages :+ WorkflowValidationMessage(s"The input schema from step ${input.stepName} is not valid.", name))
-      }
+    val schemaValidations = inputsModel.inputSchemas.headOption.map { _ =>
+      inputsModel
+        .inputSchemas
+        .flatMap(input => Seq(
+          !SdkSchemaHelper.isCorrectTableName(input.stepName) -> s"The input table name ${input.stepName} is not valid.",
+          parserInputSchema(input.schema).isFailure -> s"The input schema from step ${input.stepName} is not valid."
+        )
+        )
+    }.getOrElse(Seq.empty)
 
-      inputsModel.inputSchemas.filterNot(is => SdkSchemaHelper.isCorrectTableName(is.stepName)).foreach { is =>
-        validation = ErrorValidations(
-          valid = false,
-          messages = validation.messages :+ WorkflowValidationMessage(s"The input table name ${is.stepName} is not valid.", name))
-      }
-    }
+    val emptyColumnsValidation: Seq[(HasError, String)] = columns.map(c => c.query.isEmpty -> "New column value is not valid.")
+    val columnNameValidation: Seq[(HasError, String)] = columns.map(c => (c.field.isEmpty || c.field.contains(" ")) -> "New column name is not valid.")
 
-    if (Try(newColumnsSchemaTypes.nonEmpty).isFailure) {
-      validation = ErrorValidations(
-        valid = false,
-        messages = validation.messages :+ WorkflowValidationMessage(s"Column value and type do not match", name))
-    }
+    val validationSeq: Seq[(HasError, String)] = Seq(
+      !SdkSchemaHelper.isCorrectTableName(name) -> s"The step name $name is not valid.",
+      (selectType == SelectType.COLUMNS && columns.isEmpty) -> "It's mandatory to specify at least one column.",
+      Try(newColumnsSchemaTypes.nonEmpty).isFailure -> "Column value and type do not match"
+    ) ++ emptyColumnsValidation ++ columnNameValidation ++ schemaValidations ++ validateExpression
 
-    if (columns.isEmpty)
-      validation = ErrorValidations(
-        valid = false,
-        messages = validation.messages :+ WorkflowValidationMessage(s"It's mandatory to specify at least one column.", name)
-      )
-
-    columns.filter(c => c.field.isEmpty || c.field.contains(" ")).foreach(col => validation = ErrorValidations(
-      valid = false,
-      messages = validation.messages :+ WorkflowValidationMessage(s"New column name is not valid.", name)
-    ))
-
-    columns.filter(c => c.query.isEmpty).foreach(col => validation = ErrorValidations(
-      valid = false,
-      messages = validation.messages :+ WorkflowValidationMessage(s"New column value is not valid.", name)
-    ))
-
-    validation
+    ErrorValidationsHelper.validate(validationSeq, name)
   }
 
   def applyValues(rdd: RDD[Row], inputStep: String): (RDD[Row], Option[StructType], Option[StructType]) = {
@@ -113,11 +119,21 @@ abstract class AddColumnsTransformStep[Underlying[Row]](
       val inputSchema = getSchemaFromSessionOrModelOrRdd(xDSession, inputStep, inputsModel, rdd)
       createOrReplaceTemporalViewDf(xDSession, rdd, inputStep, inputSchema) match {
         case Some(df) =>
-          val newDataFrame = columns.foldLeft(df) {
-            (df, defValue) => {
-              import org.apache.spark.sql.functions._
-              df.withColumn(defValue.field, lit(newColumnsSchemaTypes.getOrElse(defValue.field,"")))
-            }
+          val newDataFrame = selectType match {
+
+            case SelectType.COLUMNS =>
+              columns.foldLeft(df) { (df, defValue) =>
+                import org.apache.spark.sql.functions._
+                df.withColumn(defValue.field, lit(newColumnsSchemaTypes.getOrElse(defValue.field, "")))
+              }
+
+            case SelectType.EXPRESSION =>
+              expressionList.foldLeft(df) { (dfState, columnToAdd) =>
+                import org.apache.spark.sql.functions._
+                df.withColumn(columnToAdd.addColumnAlias, expr(columnToAdd.addColumnExpression))
+              }
+
+
           }
           (newDataFrame.rdd, Option(newDataFrame.schema), inputSchema)
         case None =>
