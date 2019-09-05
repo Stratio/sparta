13786091194c5ef2a02f6550e512e5f42da244f1
leonardo-stratio <50327364+leonardo-stratio@users.noreply.github.com>
2019-09-05 07:54:15 +0200

    [SPARTA-3402] added notification each 30s when home polling fails (#2032)

--- web/src/app/modules/executions/executions-monitoring/effects/executions.ts
+++ web/src/app/modules/executions/executions-monitoring/effects/executions.ts
@@ -6,39 +6,65 @@
 
 import { Injectable } from '@angular/core';
 import { Effect, Actions, ofType } from '@ngrx/effects';
-import { Store } from '@ngrx/store';
+import { Action } from '@ngrx/store';
 
 import { Observable, timer, of } from 'rxjs';
 
 import * as executionsActions from '../actions/executions';
-import * as fromRoot from '../reducers';
 import { ExecutionService } from 'services/execution.service';
 
 import { ExecutionHelperService } from 'app/services/helpers/execution.service';
 import { catchError, concatMap, switchMap, takeUntil, map } from 'rxjs/operators';
 import { ExecutionPeriodsService } from '../services/execution-periods.service';
+import { StAlertsService, STALERT_SEVERITY } from '@stratio/egeo';
 
 
 @Injectable()
 export class ExecutionsEffect {
+  private _pollingError: boolean = false;
+  private _lastErrorTime: Date;
+  private _firstError: boolean = false;
 
   @Effect()
-  getExecutionsList$: Observable<any> = this.actions$
+  getExecutionsList$: Observable<Action> = this.actions$
     .pipe(ofType(executionsActions.LIST_EXECUTIONS))
     .pipe(switchMap(() => timer(0, 5000)
       .pipe(takeUntil(this.actions$.pipe(ofType(executionsActions.CANCEL_EXECUTION_POLLING))))
-      .pipe(concatMap(() => this._executionService.getDashboardExecutions()
-        .pipe(map((executions: any) => {
-          return new executionsActions.ListExecutionsCompleteAction({
-            executionsSummary: executions.executionsSummary,
-            executionList: executions.lastExecutions.map(execution =>
-              this._executionHelperService.normalizeExecution(execution))
-          });
-        })).pipe(
-          catchError(err => of(new executionsActions.ListExecutionsFailAction()))
-        ))
+      .pipe(concatMap(() =>
+        this._executionService.getDashboardExecutions()
+          .pipe(map((executions: any) => {
+            this._pollingError = false;
+            this._firstError = false;
+            return new executionsActions.ListExecutionsCompleteAction({
+                executionsSummary: executions.executionsSummary,
+                executionList: executions.lastExecutions.map(execution =>
+                  this._executionHelperService.normalizeExecution(execution))
+              });
+          }),
+        catchError( () => {
+          const currentTime: Date = new Date();
+          this._pollingError = true;
+
+          if (!this._lastErrorTime) {
+            this._lastErrorTime = new Date();
+          }
+
+          if ((currentTime.getTime() - this._lastErrorTime.getTime()) > 30000 || !this._firstError) {
+            this._alertsService.notifyAlert(
+              'Error',
+              'There was an internal server error',
+              STALERT_SEVERITY.ERROR,
+              null,
+              5000);
+
+            this._lastErrorTime = new Date();
+            this._firstError = true;
+          }
+
+          return of(new executionsActions.ListExecutionsFailAction());
+        })
       ))
-    );
+    )));
 
   @Effect()
   getExecutionPeriodData: Observable<any> = this.actions$
@@ -56,6 +82,7 @@ export class ExecutionsEffect {
 
   constructor(
     private actions$: Actions,
+    private _alertsService: StAlertsService,
     private _executionHelperService: ExecutionHelperService,
     private _executionPeriodsService: ExecutionPeriodsService,
     private _executionService: ExecutionService
